18a7df2b4d5c2d5ff19b158bf580226b872b7663
Mikyo King <mikyo@arize.com>
2024-01-05 16:05:13 -0700

    feat(js): OpenAI embeddings Instrumentation (#34)

--- js/packages/openinference-instrumentation-openai/test/openai.test.ts
+++ js/packages/openinference-instrumentation-openai/test/openai.test.ts
@@ -21,18 +21,22 @@ describe("OpenAIInstrumentation", () => {
 
   instrumentation.setTracerProvider(tracerProvider);
   tracerProvider.addSpanProcessor(new SimpleSpanProcessor(memoryExporter));
+  // @ts-expect-error the moduleExports property is private. This is needed to make the test work with auto-mocking
+  instrumentation._modules[0].moduleExports = OpenAI;
 
-  beforeEach(() => {
-    // @ts-expect-error the moduleExports property is private. This is needed to make the test work with auto-mocking
-    instrumentation._modules[0].moduleExports = OpenAI;
+  beforeAll(() => {
     instrumentation.enable();
     openai = new OpenAI.OpenAI({
       apiKey: `fake-api-key`,
     });
+  });
+  afterAll(() => {
+    instrumentation.disable();
+  });
+  beforeEach(() => {
     memoryExporter.reset();
   });
   afterEach(() => {
-    instrumentation.disable();
     jest.clearAllMocks();
   });
   it("is patched", () => {
@@ -85,7 +89,7 @@ describe("OpenAIInstrumentation", () => {
         "llm.input_messages.0.message.content": "Say this is a test",
         "llm.input_messages.0.message.role": "user",
         "llm.invocation_parameters": "{"model":"gpt-3.5-turbo"}",
-        "llm.model_name": "gpt-3.5-turbo",
+        "llm.model_name": "gpt-3.5-turbo-0613",
         "llm.output_messages.0.message.content": "This is a test.",
         "llm.output_messages.0.message.role": "assistant",
         "llm.token_count.completion": 5,
@@ -94,6 +98,41 @@ describe("OpenAIInstrumentation", () => {
         "openinference.span.kind": "llm",
         "output.mime_type": "application/json",
         "output.value": "{"id":"chatcmpl-8adq9JloOzNZ9TyuzrKyLpGXexh6p","object":"chat.completion","created":1703743645,"model":"gpt-3.5-turbo-0613","choices":[{"index":0,"message":{"role":"assistant","content":"This is a test."},"logprobs":null,"finish_reason":"stop"}],"usage":{"prompt_tokens":12,"completion_tokens":5,"total_tokens":17}}",
+      }
+    `);
+  });
+  it("creates a span for embedding create", async () => {
+    const response = {
+      object: "list",
+      data: [{ object: "embedding", index: 0, embedding: [1, 2, 3] }],
+    };
+    // Mock out the embedding create endpoint
+    jest.spyOn(openai, "post").mockImplementation(
+      // @ts-expect-error the response type is not correct - this is just for testing
+      async (): Promise<unknown> => {
+        return response;
+      },
+    );
+    await openai.embeddings.create({
+      input: "A happy moment",
+      model: "text-embedding-ada-002",
+    });
+    const spans = memoryExporter.getFinishedSpans();
+    expect(spans.length).toBe(1);
+    const span = spans[0];
+    expect(span.name).toBe("OpenAI Embeddings");
+    expect(span.attributes).toMatchInlineSnapshot(`
+      {
+        "embedding.embeddings.0.embedding.text": "A happy moment",
+        "embedding.embeddings.0.embedding.vector": [
+          1,
+          2,
+          3,
+        ],
+        "embedding.model_name": "text-embedding-ada-002",
+        "input.mime_type": "text/plain",
+        "input.value": "A happy moment",
+        "openinference.span.kind": "embedding",
       }
     `);
   });
