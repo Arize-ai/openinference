24570446b79d146823b17b7907310fbb23859c92
Roger Yang <roger.yang@arize.com>
2024-01-09 12:59:21 -0800

    wip

--- python/openinference-semantic-conventions/tests/openinference/semconv/trace/test_version.py
+++
@@ -1,8 +0,0 @@
-"""
-This is a dummy test to ensure that every package has one test.
-"""
-from openinference.semconv.version import __version__ as semconv_version
-
-
-def test_version():
-    print(semconv_version)
