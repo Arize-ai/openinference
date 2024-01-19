cd43dbd2cfc695c80b2dc3043473784ec712f374
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2024-01-19 14:51:07 -0800

    chore: include tests for mypy (#122)

--- python/openinference-semantic-conventions/tests/openinference/semconv/test_version.py
+++ python/openinference-semantic-conventions/tests/openinference/semconv/test_version.py
@@ -4,5 +4,5 @@ This is a dummy test to ensure that every package has one test.
 from openinference.semconv.version import __version__ as semconv_version
 
 
-def test_version():
+def test_version() -> None:
     print(semconv_version)
