ede3840267ad25e73db488c6a4bec30d4a0eaabc
Dustin Ngo <dustin@arize.com>
2024-01-22 09:51:49 -0500

    Add boto example

---
+++ python/instrumentation/openinference-instrumentation-boto/examples/boto_example.py
@@ -0,0 +1,31 @@
+import json
+
+import boto3
+from openinference.instrumentation.boto import BotoInstrumentor
+from opentelemetry import trace as trace_api
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk import trace as trace_sdk
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+
+# TODO: consider switching to an in-memory exporter
+resource = Resource(attributes={})
+tracer_provider = trace_sdk.TracerProvider(resource=resource)
+span_exporter = OTLPSpanExporter(endpoint="http://127.0.0.1:6006/v1/traces")
+span_processor = SimpleSpanProcessor(span_exporter=span_exporter)
+tracer_provider.add_span_processor(span_processor=span_processor)
+trace_api.set_tracer_provider(tracer_provider=tracer_provider)
+
+BotoInstrumentor().instrument()
+
+session = boto3.session.Session()
+client = session.client("bedrock-runtime")
+
+
+if __name__ == "__main__":
+    prompt = (
+        b'{"prompt": "Human: Hello there, how are you? Assistant:", "max_tokens_to_sample": 1024}'
+    )
+    response = client.invoke_model(modelId="anthropic.claude-v2", body=prompt)
+    response_body = json.loads(response.get("body").read())
+    print(response_body["completion"])
