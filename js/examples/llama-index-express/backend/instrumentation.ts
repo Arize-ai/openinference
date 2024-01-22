682678e57e038632f275287b2a817cb62b241d87
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2024-01-22 15:47:28 -0800

    chore(js): create-llama express app example (#128)
    Co-authored-by: Mikyo King <mikyo@arize.com>
    Co-authored-by: Alexander Song <axiomofjoy@gmail.com>

--- js/examples/llama-index-express/backend/instrumentation.ts
+++ js/examples/llama-index-express/backend/instrumentation.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { OpenAIInstrumentation } from "@arizeai/openinference-instrumentation-openai";
 import {
@@ -8,6 +9,10 @@ import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
 import { Resource } from "@opentelemetry/resources";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
+
+// For troubleshooting, set the log level to DiagLogLevel.DEBUG
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 const provider = new NodeTracerProvider({
   resource: new Resource({
@@ -23,8 +28,11 @@ provider.addSpanProcessor(
     }),
   ),
 );
-provider.register();
 
 registerInstrumentations({
   instrumentations: [new OpenAIInstrumentation({})],
 });
+
+provider.register();
+
+console.log("👀 OpenInference initialized");
