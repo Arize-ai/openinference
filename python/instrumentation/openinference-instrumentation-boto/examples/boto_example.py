990954fdd9e577144114e72324ec21bca12eb36b
Dustin Ngo <dustin@arize.com>
2024-01-24 10:39:55 -0500

    Add console exporter

--- python/instrumentation/openinference-instrumentation-boto/examples/boto_example.py
+++ python/instrumentation/openinference-instrumentation-boto/examples/boto_example.py
@@ -6,13 +6,14 @@ from opentelemetry import trace as trace_api
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk import trace as trace_sdk
 from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 resource = Resource(attributes={})
 tracer_provider = trace_sdk.TracerProvider(resource=resource)
-span_exporter = OTLPSpanExporter(endpoint="http://127.0.0.1:6006/v1/traces")
-span_processor = SimpleSpanProcessor(span_exporter=span_exporter)
-tracer_provider.add_span_processor(span_processor=span_processor)
+span_console_exporter = ConsoleSpanExporter()
+span_otlp_exporter = OTLPSpanExporter(endpoint="http://127.0.0.1:6006/v1/traces")
+tracer_provider.add_span_processor(SimpleSpanProcessor(span_exporter=span_console_exporter))
+tracer_provider.add_span_processor(SimpleSpanProcessor(span_exporter=span_otlp_exporter))
 trace_api.set_tracer_provider(tracer_provider=tracer_provider)
 
 BotoInstrumentor().instrument()
