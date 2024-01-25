23b0efbbc41de4998f60a2e53d70d254c4ab3ce8
Roger Yang <roger.yang@arize.com>
2024-01-25 09:35:15 -0800

    clean up

--- python/instrumentation/openinference-instrumentation-langchain/src/openinference/instrumentation/langchain/__init__.py
+++ python/instrumentation/openinference-instrumentation-langchain/src/openinference/instrumentation/langchain/__init__.py
@@ -9,18 +9,14 @@ from opentelemetry import trace as trace_api
 from opentelemetry.instrumentation.instrumentor import BaseInstrumentor  # type: ignore
 from wrapt import wrap_function_wrapper
 
-_MODULE = "langchain"
-
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
 class LangChainInstrumentor(BaseInstrumentor):  # type: ignore
     """
-    An instrumentor for LlamaIndex
+    An instrumentor for LangChain
     """
-
-    __slots__ = ("_original_global_handler",)
 
     def instrumentation_dependencies(self) -> Collection[str]:
         return _instruments
@@ -29,7 +25,6 @@ class LangChainInstrumentor(BaseInstrumentor):  # type: ignore
         if not (tracer_provider := kwargs.get("tracer_provider")):
             tracer_provider = trace_api.get_tracer_provider()
         tracer = trace_api.get_tracer(__name__, __version__, tracer_provider)
-
         wrap_function_wrapper(
             "langchain_core.callbacks",
             "BaseCallbackManager.__init__",
