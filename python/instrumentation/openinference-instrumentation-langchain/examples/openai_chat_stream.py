67345cf5ab3479fd536a041b837b2ee9d2ebe599
Roger Yang <roger.yang@arize.com>
2024-01-25 13:47:22 -0800

    clean up

--- python/instrumentation/openinference-instrumentation-langchain/examples/openai_chat_stream.py
+++ python/instrumentation/openinference-instrumentation-langchain/examples/openai_chat_stream.py
@@ -1,3 +1,5 @@
+import os
+
 from langchain_openai import ChatOpenAI
 from openinference.instrumentation.langchain import LangChainInstrumentor
 from opentelemetry import trace as trace_api
@@ -8,11 +10,16 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 
 resource = Resource(attributes={})
 tracer_provider = trace_sdk.TracerProvider(resource=resource)
-span_otlp_exporter = OTLPSpanExporter(endpoint="http://127.0.0.1:6006/v1/traces")
-tracer_provider.add_span_processor(SimpleSpanProcessor(span_exporter=span_otlp_exporter))
-span_console_exporter = ConsoleSpanExporter()
-tracer_provider.add_span_processor(SimpleSpanProcessor(span_exporter=span_console_exporter))
 trace_api.set_tracer_provider(tracer_provider=tracer_provider)
+
+# Logs to the Phoenix Collector if running locally
+if os.environ.get("PHOENIX_COLLECTOR_ENDPOINT"):
+    endpoint = os.environ["PHOENIX_COLLECTOR_ENDPOINT"] + "/v1/traces"
+    span_otlp_exporter = OTLPSpanExporter(endpoint=endpoint)
+    tracer_provider.add_span_processor(SimpleSpanProcessor(span_exporter=span_otlp_exporter))
+else:
+    span_console_exporter = ConsoleSpanExporter()
+    tracer_provider.add_span_processor(SimpleSpanProcessor(span_exporter=span_console_exporter))
 
 LangChainInstrumentor().instrument()
 
