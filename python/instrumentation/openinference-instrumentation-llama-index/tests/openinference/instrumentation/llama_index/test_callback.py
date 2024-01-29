1feac1e834ce91fdf1fbcbd336c2e44b21e5d65a
Roger Yang <roger.yang@arize.com>
2024-01-29 12:44:14 -0800

    clean up

--- python/instrumentation/openinference-instrumentation-llama-index/tests/openinference/instrumentation/llama_index/test_callback.py
+++ python/instrumentation/openinference-instrumentation-llama-index/tests/openinference/instrumentation/llama_index/test_callback.py
@@ -25,7 +25,6 @@ from openinference.semconv.trace import (
     ToolCallAttributes,
 )
 from opentelemetry import trace as trace_api
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk import trace as trace_sdk
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace.export import SimpleSpanProcessor
@@ -259,11 +258,6 @@ def tracer_provider(in_memory_span_exporter: InMemorySpanExporter) -> trace_api.
     tracer_provider = trace_sdk.TracerProvider(resource=resource)
     span_processor = SimpleSpanProcessor(span_exporter=in_memory_span_exporter)
     tracer_provider.add_span_processor(span_processor=span_processor)
-    tracer_provider.add_span_processor(
-        span_processor=SimpleSpanProcessor(
-            span_exporter=OTLPSpanExporter(endpoint="http://127.0.0.1:6006/v1/traces")
-        )
-    )
     return tracer_provider
 
 
