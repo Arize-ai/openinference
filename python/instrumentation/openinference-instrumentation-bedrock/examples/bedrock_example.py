a488362263b5d18d3cb7f80138a12215c4185c89
Dustin Ngo <dustin@arize.com>
2024-02-07 11:28:53 -0500

    Add bedrock tracing tutorial notebook

--- python/instrumentation/openinference-instrumentation-bedrock/examples/bedrock_example.py
+++ python/instrumentation/openinference-instrumentation-bedrock/examples/bedrock_example.py
@@ -3,7 +3,6 @@ import json
 import boto3
 from openinference.instrumentation.bedrock import BedrockInstrumentor
 from opentelemetry import trace as trace_api
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk import trace as trace_sdk
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
@@ -11,9 +10,7 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 resource = Resource(attributes={})
 tracer_provider = trace_sdk.TracerProvider(resource=resource)
 span_console_exporter = ConsoleSpanExporter()
-span_otlp_exporter = OTLPSpanExporter(endpoint="http://127.0.0.1:6006/v1/traces")
 tracer_provider.add_span_processor(SimpleSpanProcessor(span_exporter=span_console_exporter))
-tracer_provider.add_span_processor(SimpleSpanProcessor(span_exporter=span_otlp_exporter))
 trace_api.set_tracer_provider(tracer_provider=tracer_provider)
 
 BedrockInstrumentor().instrument()
