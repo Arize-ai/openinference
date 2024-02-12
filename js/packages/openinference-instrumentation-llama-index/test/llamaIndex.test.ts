30deaee3340d4b63b514155f505bb6d5af51d7c6
Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>
2024-02-12 13:34:01 -0800

    feat(llamaindex): scaffold LlamaIndex.TS instrumentor package (#199)

---
+++ js/packages/openinference-instrumentation-llama-index/test/llamaIndex.test.ts
@@ -0,0 +1,8 @@
+/**
+ * Dummy test for scaffolding package, remove when real tests are added
+ */
+describe("llamaIndex", () => {
+  it("should pass", () => {
+    expect(true).toBe(true);
+  });
+});
