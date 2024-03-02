cad5717256223567bdbaeb3e44371f79221dae74
Alexander Song <axiomofjoy@gmail.com>
2024-03-01 16:23:04 -0800

    fix signature name

--- python/instrumentation/openinference-instrumentation-dspy/src/openinference/instrumentation/dspy/__init__.py
+++ python/instrumentation/openinference-instrumentation-dspy/src/openinference/instrumentation/dspy/__init__.py
@@ -2,7 +2,7 @@ import json
 from abc import ABC
 from enum import Enum
 from inspect import signature
-from typing import Any, Callable, Collection, Dict, Iterator, List, Mapping, Tuple
+from typing import Any, Callable, Collection, Dict, Iterator, List, Mapping, Optional, Tuple
 
 from openinference.instrumentation.dspy.package import _instruments
 from openinference.instrumentation.dspy.version import __version__
@@ -485,10 +485,22 @@ def _get_predict_span_name(instance: Any) -> str:
     """
     class_name = str(instance.__class__.__name__)
     if (signature := getattr(instance, "signature", None)) and (
-        signature_name := getattr(signature, "__name__", None)
+        signature_name := _get_signature_name(signature)
     ):
         return f"{class_name}({signature_name}).forward"
     return f"{class_name}.forward"
+
+
+def _get_signature_name(signature: Any) -> Optional[str]:
+    """
+    A best-effort attempt to get the name of a signature.
+    """
+    if (
+        # At the time of this writing, the __name__ attribute on signatures does
+        # not return the user-defined class name, but __qualname__ does.
+        qual_name := getattr(signature, "__qualname__", None)
+    ) is not None:
+        return qual_name.split(".")[-1]
 
 
 def _flatten(mapping: Mapping[str, Any]) -> Iterator[Tuple[str, AttributeValue]]:
