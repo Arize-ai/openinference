8d1f735605e3a02d46a4bdcb7fa2d110123362f9
Roger Yang <roger.yang@arize.com>
2024-03-20 11:05:40 -0700

    clean up

--- python/instrumentation/openinference-instrumentation-langchain/src/openinference/instrumentation/langchain/_tracer.py
+++ python/instrumentation/openinference-instrumentation-langchain/src/openinference/instrumentation/langchain/_tracer.py
@@ -208,13 +208,16 @@ def _serialize_json(obj: Any) -> str:
 def stop_on_exception(
     wrapped: Callable[..., Iterator[Tuple[str, Any]]],
 ) -> Callable[..., Iterator[Tuple[str, Any]]]:
-    wrapped = audit_timing(wrapped)
-
     def wrapper(*args: Any, **kwargs: Any) -> Iterator[Tuple[str, Any]]:
+        start_time = time.perf_counter()
         try:
             yield from wrapped(*args, **kwargs)
         except Exception:
             logger.exception("Failed to get attribute.")
+        finally:
+            if _AUDIT_TIMING:
+                latency_ms = (time.perf_counter() - start_time) * 1000
+                print(f"{wrapped.__name__}: {latency_ms:.3f}ms")
 
     return wrapper
 
