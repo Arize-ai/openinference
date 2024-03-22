b7b314d4d38a1995d38989fc5169ddbf2c0bebfa
Alexander Song <axiomofjoy@gmail.com>
2024-03-21 20:05:54 -0700

    add example for chat completions with function calling

---
+++ python/instrumentation/openinference-instrumentation-openai/examples/chat_completions_with_function_calling.py
@@ -0,0 +1,76 @@
+import openai
+from openai.types.chat import (
+    ChatCompletionAssistantMessageParam,
+    ChatCompletionMessageToolCallParam,
+    ChatCompletionToolMessageParam,
+)
+from openinference.instrumentation.openai import OpenAIInstrumentor
+from opentelemetry import trace as trace_api
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk import trace as trace_sdk
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+
+endpoint = "http://127.0.0.1:6006/v1/traces"
+tracer_provider = trace_sdk.TracerProvider()
+tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
+tracer_provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
+trace_api.set_tracer_provider(tracer_provider)
+
+OpenAIInstrumentor().instrument()
+
+
+if __name__ == "__main__":
+    client = openai.OpenAI()
+    response = client.chat.completions.create(
+        model="gpt-4",
+        tools=[
+            {
+                "type": "function",
+                "function": {
+                    "name": "get_weather",
+                    "description": "finds the weather for a given city",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "city": {
+                                "type": "string",
+                                "description": "The city to find the weather for, e.g. 'London'",
+                            }
+                        },
+                        "required": ["city"],
+                    },
+                },
+            },
+        ],
+        messages=[
+            {
+                "role": "user",
+                "content": "What's the weather like in San Francisco?",
+            },
+        ],
+    )
+    message = response.choices[0].message
+    assert (tool_calls := message.tool_calls)
+    tool_call_id = tool_calls[0].id
+    client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"content": "What's the weather like in San Francisco?", "role": "user"},
+            ChatCompletionAssistantMessageParam(
+                role="assistant",
+                content="",
+                tool_calls=[
+                    ChatCompletionMessageToolCallParam(
+                        id=tool_call_id,
+                        function={"name": "get_weather", "arguments": '{"city": "San Francisco"}'},
+                        type="function",
+                    ),
+                ]
+            ),
+            ChatCompletionToolMessageParam(
+                content='{"weather_category": "sunny"}',
+                role="tool",
+                tool_call_id=tool_call_id
+            )
+        ],
+    )
