67e4949dc27927944dfed56c999e2df0bfd64976
Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>
2024-04-02 16:37:01 -0700

    feat: scaffold langchainjs instrumentation package (#357)

---
+++ js/packages/openinference-instrumentation-langchain/test/langchain.test.ts
@@ -0,0 +1,8 @@
+/**
+ * Dummy test for scaffolding package, remove when real tests are added
+ */
+describe("langchain", () => {
+  it("should pass", () => {
+    expect(true).toBe(true);
+  });
+});
