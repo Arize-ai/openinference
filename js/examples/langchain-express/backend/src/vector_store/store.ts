5daed3c7d27f7cf31a93393eeb0112d919eec578
Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>
2024-04-23 08:45:36 -0700

    docs: update LangChain.js instrumentation docs and improve rag in example (#396)

--- js/examples/langchain-express/backend/src/vector_store/store.ts
+++ js/examples/langchain-express/backend/src/vector_store/store.ts
@@ -4,16 +4,27 @@ import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import "cheerio";
 import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { Document } from "@langchain/core/documents";
+import { DOCUMENT_URLS, WEB_LOADER_TIMEOUT } from "../constants";
 
 export const initializeVectorStore = async () => {
-  const loader = new CheerioWebBaseLoader(
-    "https://docs.arize.com/phoenix/programming-languages/javascript",
+  const loader = new CheerioWebBaseLoader("https://docs.arize.com/phoenix");
+  const webContent = await CheerioWebBaseLoader.scrapeAll(
+    DOCUMENT_URLS,
+    loader.caller,
+    WEB_LOADER_TIMEOUT,
   );
 
-  const documents = await loader.load();
+  const documents = webContent.map((doc, i) => {
+    const text = doc("body").text();
+    return new Document({
+      pageContent: text,
+      metadata: { source: DOCUMENT_URLS[i] },
+    });
+  });
   const textSplitter = new RecursiveCharacterTextSplitter({
-    chunkOverlap: 200,
-    chunkSize: 1000,
+    chunkOverlap: 100,
+    chunkSize: 500,
   });
 
   const splits = await textSplitter.splitDocuments(documents);
