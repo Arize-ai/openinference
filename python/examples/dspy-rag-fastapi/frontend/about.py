2d866f8dcc132474382f50e14b785374357e0871
Mikyo King <mikyo@arize.com>
2024-04-24 22:47:26 -0600

    frontend

--- python/examples/dspy-rag-fastapi/frontend/about.py
+++ python/examples/dspy-rag-fastapi/frontend/about.py
@@ -14,12 +14,19 @@ st.sidebar.info("Select a demo above.")
 
 st.markdown(
     """
-    ## Description
-    This project introduces a [Streamlit](https://streamlit.io) application designed to interface seamlessly with the [DSPy](https://github.com/stanfordnlp/dspy) framework by StanfordNLP, encapsulated within a [FastAPI](https://github.com/tiangolo/fastapi) backend. It offers an intuitive and interactive frontend solution, showcasing the capabilities of DSPy through a user-friendly web interface. The application leverages [Ollama](https://github.com/ollama/ollama) for language and embedding models, [Chroma DB](https://github.com/chroma-core/chroma) for vector storage, and [Arize Phoenix](https://github.com/Arize-ai/phoenix) for an observability layer.
+    ## Description This project introduces a [Streamlit](https://streamlit.io)
+    application designed to interface seamlessly with the
+    [DSPy](https://github.com/stanfordnlp/dspy) framework by StanfordNLP,
+    encapsulated within a [FastAPI](https://github.com/tiangolo/fastapi)
+    backend. It offers an intuitive and interactive frontend solution,
+    showcasing the capabilities of DSPy through a user-friendly web interface.
+    This application's traces are captured by [Arize
+    Phoenix](https://github.com/Arize-ai/phoenix) for observability.
 
-    In a notable application of this system, the Retrieval-Augmented Generation (RAG) process is performed on the insightful essay "What I Worked On" by Paul Graham. 
+    In a notable application of this system, the Retrieval-Augmented Generation
+    (RAG) process is performed on the insightful essay "What I Worked On" by
+    Paul Graham. 
 
-    ### Info
-    [GitHub](https://github.com/diicellman/dspy-rag-fastapi)
+    ### Info [GitHub](https://github.com/Arize-ai/openinference)
 """
 )
