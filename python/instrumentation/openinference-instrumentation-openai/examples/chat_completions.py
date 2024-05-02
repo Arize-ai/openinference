7f89fe2fef99e8f24110433a44adaf34d8cc1430
Francisco Castillo <kiko@arize.com>
2024-05-02 13:07:33 -0700

    Add context manager to examples

--- python/instrumentation/openinference-instrumentation-openai/examples/chat_completions.py
+++ python/instrumentation/openinference-instrumentation-openai/examples/chat_completions.py
@@ -1,17 +1,14 @@
 import openai
+from openinference.instrumentation import using_attributes
 from openinference.instrumentation.openai import OpenAIInstrumentor
-
-# from openinference.instrumentation import using_attributes
-from openinference_instrumentation import using_attributes
 from opentelemetry import trace as trace_api
-
-# from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk import trace as trace_sdk
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 endpoint = "http://127.0.0.1:6006/v1/traces"
 tracer_provider = trace_sdk.TracerProvider()
-# tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
+tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
 tracer_provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
 trace_api.set_tracer_provider(tracer_provider)
 
@@ -20,9 +17,23 @@ OpenAIInstrumentor().instrument()
 
 if __name__ == "__main__":
     client = openai.OpenAI()
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": "Write a haiku."}],
-        max_tokens=20,
-    )
-    print(response.choices[0].message.content)
+    with using_attributes(
+        session_id="my-test-session",
+        user_id="my-test-user",
+        metadata={
+            "test-int": 1,
+            "test-str": "string",
+            "test-list": [1, 2, 3],
+            "test-dict": {
+                "key-1": "val-1",
+                "key-2": "val-2",
+            },
+        },
+        tags=["tag-1", "tag-2"],
+    ):
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": "Write a haiku."}],
+            max_tokens=20,
+        )
+        print(response.choices[0].message.content)
