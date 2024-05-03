0005fa8025a6c5bc44535f6610de4b938b535125
Kiko Castillo <fjcasti1@asu.edu>
2024-05-02 20:43:56 -0700

    feat: Make langchain instrumentation read context attributes (#419)
    Co-authored-by: Roger Yang <80478925+RogerHYang@users.noreply.github.com>

--- python/instrumentation/openinference-instrumentation-langchain/examples/chain_metadata.py
+++ python/instrumentation/openinference-instrumentation-langchain/examples/chain_metadata.py
@@ -1,6 +1,7 @@
 from langchain.chains import LLMChain
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAI
+from openinference.instrumentation import using_attributes
 from openinference.instrumentation.langchain import LangChainInstrumentor
 from opentelemetry import trace as trace_api
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
@@ -17,8 +18,22 @@ LangChainInstrumentor().instrument()
 
 
 if __name__ == "__main__":
-    prompt_template = "Tell me a {adjective} joke"
-    prompt = PromptTemplate(input_variables=["adjective"], template=prompt_template)
-    llm = LLMChain(llm=OpenAI(), prompt=prompt, metadata={"category": "jokes"})
-    completion = llm.predict(adjective="funny", metadata={"variant": "funny"})
-    print(completion)
+    with using_attributes(
+        session_id="my-test-session",
+        user_id="my-test-user",
+        metadata={
+            "test-int": 1,
+            "test-str": "string",
+            "test-list": [1, 2, 3],
+            "test-dict": {
+                "key-1": "val-1",
+                "key-2": "val-2",
+            },
+        },
+        tags=["tag-1", "tag-2"],
+    ):
+        prompt_template = "Tell me a {adjective} joke"
+        prompt = PromptTemplate(input_variables=["adjective"], template=prompt_template)
+        llm = LLMChain(llm=OpenAI(), prompt=prompt, metadata={"category": "jokes"})
+        completion = llm.predict(adjective="funny", metadata={"variant": "funny"})
+        print(completion)
