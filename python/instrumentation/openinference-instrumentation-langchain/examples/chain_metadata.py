4652c9df0ad98ead9a61a14ab5460231ee7471c7
Francisco Castillo <kiko@arize.com>
2024-05-02 18:41:13 -0700

    wip

--- python/instrumentation/openinference-instrumentation-langchain/examples/chain_metadata.py
+++ python/instrumentation/openinference-instrumentation-langchain/examples/chain_metadata.py
@@ -4,13 +4,14 @@ from langchain_openai import OpenAI
 from openinference.instrumentation import using_attributes
 from openinference.instrumentation.langchain import LangChainInstrumentor
 from opentelemetry import trace as trace_api
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk import trace as trace_sdk
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 endpoint = "http://127.0.0.1:6006/v1/traces"
 tracer_provider = trace_sdk.TracerProvider()
 trace_api.set_tracer_provider(tracer_provider)
-# tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
+tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
 tracer_provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
 
 LangChainInstrumentor().instrument()
