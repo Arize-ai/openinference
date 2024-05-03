2bed41c6af344ebf304a93f7a4ba7a0839bd0cbb
Francisco Castillo <kiko@arize.com>
2024-05-02 19:13:15 -0700

    wip

--- python/instrumentation/openinference-instrumentation-langchain/examples/chain_metadata.py
+++ python/instrumentation/openinference-instrumentation-langchain/examples/chain_metadata.py
@@ -1,7 +1,6 @@
 from langchain.chains import LLMChain
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAI
-from openinference.instrumentation import using_attributes
 from openinference.instrumentation.langchain import LangChainInstrumentor
 from opentelemetry import trace as trace_api
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
@@ -18,22 +17,8 @@ LangChainInstrumentor().instrument()
 
 
 if __name__ == "__main__":
-    with using_attributes(
-        session_id="my-test-session",
-        user_id="my-test-user",
-        metadata={
-            "test-int": 1,
-            "test-str": "string",
-            "test-list": [1, 2, 3],
-            "test-dict": {
-                "key-1": "val-1",
-                "key-2": "val-2",
-            },
-        },
-        tags=["tag-1", "tag-2"],
-    ):
-        prompt_template = "Tell me a {adjective} joke"
-        prompt = PromptTemplate(input_variables=["adjective"], template=prompt_template)
-        llm = LLMChain(llm=OpenAI(), prompt=prompt, metadata={"category": "jokes"})
-        completion = llm.predict(adjective="funny", metadata={"variant": "funny"})
-        print(completion)
+    prompt_template = "Tell me a {adjective} joke"
+    prompt = PromptTemplate(input_variables=["adjective"], template=prompt_template)
+    llm = LLMChain(llm=OpenAI(), prompt=prompt, metadata={"category": "jokes"})
+    completion = llm.predict(adjective="funny", metadata={"variant": "funny"})
+    print(completion)
--- python/instrumentation/openinference-instrumentation-langchain/examples/openai_chat_stream.py
+++ python/instrumentation/openinference-instrumentation-langchain/examples/openai_chat_stream.py
@@ -1,5 +1,4 @@
 from langchain_openai import ChatOpenAI
-from openinference.instrumentation import using_attributes
 from openinference.instrumentation.langchain import LangChainInstrumentor
 from opentelemetry import trace as trace_api
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
@@ -16,19 +15,5 @@ LangChainInstrumentor().instrument()
 
 
 if __name__ == "__main__":
-    with using_attributes(
-        session_id="my-test-session",
-        user_id="my-test-user",
-        metadata={
-            "test-int": 1,
-            "test-str": "string",
-            "test-list": [1, 2, 3],
-            "test-dict": {
-                "key-1": "val-1",
-                "key-2": "val-2",
-            },
-        },
-        tags=["tag-1", "tag-2"],
-    ):
-        for chunk in ChatOpenAI(model_name="gpt-3.5-turbo").stream("Write a haiku."):
-            print(chunk.content, end="", flush=True)
+    for chunk in ChatOpenAI(model_name="gpt-3.5-turbo").stream("Write a haiku."):
+        print(chunk.content, end="", flush=True)
