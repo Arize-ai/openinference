b20c85bfef217ccaf4645cfc6bc1388b12378a4c
Kiko Castillo <fjcasti1@asu.edu>
2024-05-03 19:01:53 -0700

    feat: Allow context managers as decorators (#431)

--- python/openinference-instrumentation/tests/test_context_managers.py
+++ python/openinference-instrumentation/tests/test_context_managers.py
@@ -23,41 +23,96 @@ def test_suppress_tracing() -> None:
     assert get_value(_SUPPRESS_INSTRUMENTATION_KEY) is None
 
 
-def test_using_session() -> None:
-    with using_session("test-session"):
-        assert get_value(SpanAttributes.SESSION_ID) == "test-session"
+def test_using_session(session_id: str) -> None:
+    with using_session(session_id):
+        assert get_value(SpanAttributes.SESSION_ID) == session_id
     assert get_value(SpanAttributes.SESSION_ID) is None
 
 
-def test_using_user() -> None:
-    with using_user("test-user"):
-        assert get_value(SpanAttributes.USER_ID) == "test-user"
+def test_using_user(user_id: str) -> None:
+    with using_user(user_id):
+        assert get_value(SpanAttributes.USER_ID) == user_id
     assert get_value(SpanAttributes.USER_ID) is None
 
 
-def test_using_metadata(metadata_fixture: Dict[str, Any]) -> None:
-    with using_metadata(metadata_fixture):
-        assert get_value(SpanAttributes.METADATA) == json.dumps(metadata_fixture, default=str)
+def test_using_metadata(metadata: Dict[str, Any]) -> None:
+    with using_metadata(metadata):
+        assert get_value(SpanAttributes.METADATA) == json.dumps(metadata)
     assert get_value(SpanAttributes.METADATA) is None
 
 
-def test_using_tags(tags_fixture: List[str]) -> None:
-    with using_tags(tags_fixture):
-        assert get_value(SpanAttributes.TAG_TAGS) == tags_fixture
+def test_using_tags(tags: List[str]) -> None:
+    with using_tags(tags):
+        assert get_value(SpanAttributes.TAG_TAGS) == tags
     assert get_value(SpanAttributes.TAG_TAGS) is None
 
 
-def test_using_attributes(metadata_fixture: Dict[str, Any], tags_fixture: List[str]) -> None:
+def test_using_attributes(
+    session_id: str, user_id: str, metadata: Dict[str, Any], tags: List[str]
+) -> None:
     with using_attributes(
-        session_id="test-session",
-        user_id="test-user",
-        metadata=metadata_fixture,
-        tags=tags_fixture,
+        session_id=session_id,
+        user_id=user_id,
+        metadata=metadata,
+        tags=tags,
     ):
-        assert get_value(SpanAttributes.SESSION_ID) == "test-session"
-        assert get_value(SpanAttributes.USER_ID) == "test-user"
-        assert get_value(SpanAttributes.METADATA) == json.dumps(metadata_fixture, default=str)
-        assert get_value(SpanAttributes.TAG_TAGS) == tags_fixture
+        assert get_value(SpanAttributes.SESSION_ID) == session_id
+        assert get_value(SpanAttributes.USER_ID) == user_id
+        assert get_value(SpanAttributes.METADATA) == json.dumps(metadata)
+        assert get_value(SpanAttributes.TAG_TAGS) == tags
+    assert get_value(SpanAttributes.SESSION_ID) is None
+    assert get_value(SpanAttributes.USER_ID) is None
+    assert get_value(SpanAttributes.METADATA) is None
+    assert get_value(SpanAttributes.TAG_TAGS) is None
+
+
+def test_using_session_decorator(session_id: str) -> None:
+    @using_session(session_id)
+    def f() -> None:
+        assert get_value(SpanAttributes.SESSION_ID) == session_id
+
+    f()
+    assert get_value(SpanAttributes.SESSION_ID) is None
+
+
+def test_using_user_decorator(user_id: str) -> None:
+    @using_user(user_id)
+    def f() -> None:
+        assert get_value(SpanAttributes.USER_ID) == user_id
+
+    f()
+    assert get_value(SpanAttributes.USER_ID) is None
+
+
+def test_using_metadata_decorator(metadata: Dict[str, Any]) -> None:
+    @using_metadata(metadata)
+    def f() -> None:
+        assert get_value(SpanAttributes.METADATA) == json.dumps(metadata)
+
+    f()
+    assert get_value(SpanAttributes.METADATA) is None
+
+
+def test_using_tags_decorator(tags: List[str]) -> None:
+    @using_tags(tags)
+    def f() -> None:
+        assert get_value(SpanAttributes.TAG_TAGS) == tags
+
+    f()
+    assert get_value(SpanAttributes.TAG_TAGS) is None
+
+
+def test_using_attributes_decorator(
+    session_id: str, user_id: str, metadata: Dict[str, Any], tags: List[str]
+) -> None:
+    @using_attributes(session_id=session_id, user_id=user_id, metadata=metadata, tags=tags)
+    def f() -> None:
+        assert get_value(SpanAttributes.SESSION_ID) == session_id
+        assert get_value(SpanAttributes.USER_ID) == user_id
+        assert get_value(SpanAttributes.METADATA) == json.dumps(metadata)
+        assert get_value(SpanAttributes.TAG_TAGS) == tags
+
+    f()
     assert get_value(SpanAttributes.SESSION_ID) is None
     assert get_value(SpanAttributes.USER_ID) is None
     assert get_value(SpanAttributes.METADATA) is None
@@ -65,14 +120,24 @@ def test_using_attributes(metadata_fixture: Dict[str, Any], tags_fixture: List[s
 
 
 @pytest.fixture
-def tags_fixture() -> List[str]:
-    return ["tag_1", "tag_2"]
+def session_id() -> str:
+    return "test-session"
 
 
 @pytest.fixture
-def metadata_fixture() -> Dict[str, Any]:
+def user_id() -> str:
+    return "test-user"
+
+
+@pytest.fixture
+def metadata() -> Dict[str, Any]:
     return {
         "key-int": 1,
         "key-str": "2",
         "key-list": [1, 2, 3],
     }
+
+
+@pytest.fixture
+def tags() -> List[str]:
+    return ["tag_1", "tag_2"]
