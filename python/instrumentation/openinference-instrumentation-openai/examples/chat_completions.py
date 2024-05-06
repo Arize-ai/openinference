6c8085720e481785ef9ee8882642fa7701877bd6
Francisco Castillo <kiko@arize.com>
2024-05-06 16:59:35 -0700

    wip

--- python/instrumentation/openinference-instrumentation-openai/examples/chat_completions.py
+++ python/instrumentation/openinference-instrumentation-openai/examples/chat_completions.py
@@ -3,13 +3,13 @@ from openinference.instrumentation import using_attributes
 from openinference.instrumentation.openai import OpenAIInstrumentor
 from opentelemetry import trace as trace_api
 
-# from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk import trace as trace_sdk
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 endpoint = "http://127.0.0.1:6006/v1/traces"
 tracer_provider = trace_sdk.TracerProvider()
-# tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
+tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
 tracer_provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
 trace_api.set_tracer_provider(tracer_provider)
 
