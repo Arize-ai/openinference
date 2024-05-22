a94d06af21121c3c2a14db060b89b943ec5b2b2f
Mikyo King <mikyo@arize.com>
2024-05-21 20:04:11 -0600

    prettier

--- js/examples/nextjs-openai-simple/instrumentation-node.ts
+++ js/examples/nextjs-openai-simple/instrumentation-node.ts
@@ -15,19 +15,17 @@ import { Resource } from "@opentelemetry/resources";
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 const provider = new NodeTracerProvider({
-    resource: new Resource({
-        ["openinference.project.name"]: "nextjs-chat",
-    }),
+  resource: new Resource({
+    ["openinference.project.name"]: "nextjs-chat",
+  }),
 });
 
 provider.addSpanProcessor(
-    new SimpleSpanProcessor(
-        new OTLPTraceExporter({
-            url:
-                process.env.COLLECTOR_ENDPOINT ||
-                "http://localhost:6006/v1/traces",
-        })
-    )
+  new SimpleSpanProcessor(
+    new OTLPTraceExporter({
+      url: process.env.COLLECTOR_ENDPOINT || "http://localhost:6006/v1/traces",
+    }),
+  ),
 );
 
 // OpenAI must be manually instrumented as it doesn't have a traditional module structure
@@ -35,7 +33,7 @@ const openAIInstrumentation = new OpenAIInstrumentation({});
 openAIInstrumentation.manuallyInstrument(OpenAI);
 
 registerInstrumentations({
-    instrumentations: [openAIInstrumentation],
+  instrumentations: [openAIInstrumentation],
 });
 
 provider.register();
