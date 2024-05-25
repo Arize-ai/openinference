0d1d0655d82f6c92a06682503be78867af68710a
Mikyo King <mikyo@arize.com>
2024-05-25 13:55:47 -0600

    feat(js): add semcov resource atters for project name (#498)

---
+++ js/.changeset/heavy-dryers-happen.md
@@ -0,0 +1,5 @@
+---
+"@arizeai/openinference-semantic-conventions": minor
+---
+
+Add resource attribute for project name
---
+++ js/packages/openinference-semantic-conventions/src/resource/SemanticResourceAttributes.ts
@@ -0,0 +1,6 @@
+const PROJECT_NAME = "openinference.project.name";
+
+/**
+ * The project name to group traces under for openinference compatible services
+ */
+export const SEMRESATTRS_PROJECT_NAME = PROJECT_NAME;
