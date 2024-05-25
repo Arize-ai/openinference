03519e2f2946983e8cd8cd82fbbc312693ed7552
Mikyo King <mikyo@arize.com>
2024-05-25 12:30:19 -0600

    fix spelling

--- js/packages/openinference-semantic-conventions/src/resource/SemanticResourceAttributes.ts
+++ js/packages/openinference-semantic-conventions/src/resource/SemanticResourceAttributes.ts
@@ -3,4 +3,4 @@ const PROJECT_NAME = "openinference.project.name";
 /**
  * The project name to group traces under for openinference compatible services
  */
-export const SEMRSATTERS_PROJECT_NAME = PROJECT_NAME;
+export const SEMRESATTRS_PROJECT_NAME = PROJECT_NAME;
