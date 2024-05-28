25db24095fe5f653ecc2b53630059d38374e7dd2
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2024-05-28 10:45:33 -0700

    refactor: add token counts to openai streaming examples (#506)

--- python/instrumentation/openinference-instrumentation-openai/examples/chat_completions_async_stream.py
+++ python/instrumentation/openinference-instrumentation-openai/examples/chat_completions_async_stream.py
@@ -3,7 +3,6 @@ import asyncio
 import openai
 from openinference.instrumentation import using_attributes
 from openinference.instrumentation.openai import OpenAIInstrumentor
-from opentelemetry import trace as trace_api
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk import trace as trace_sdk
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
@@ -12,9 +11,8 @@ endpoint = "http://127.0.0.1:6006/v1/traces"
 tracer_provider = trace_sdk.TracerProvider()
 tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
 tracer_provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
-trace_api.set_tracer_provider(tracer_provider)
 
-OpenAIInstrumentor().instrument()
+OpenAIInstrumentor().instrument(tracer_provider=tracer_provider)
 
 
 async def chat_completions(**kwargs):
@@ -41,7 +39,7 @@ async def chat_completions(**kwargs):
     ):
         response = await client.chat.completions.create(**kwargs)
         async for chunk in response:
-            if content := chunk.choices[0].delta.content:
+            if chunk.choices and (content := chunk.choices[0].delta.content):
                 print(content, end="")
 
 
@@ -52,5 +50,6 @@ if __name__ == "__main__":
             messages=[{"role": "user", "content": "Write a haiku."}],
             max_tokens=20,
             stream=True,
+            stream_options={"include_usage": True},
         ),
     )
