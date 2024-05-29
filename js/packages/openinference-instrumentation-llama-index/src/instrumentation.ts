542501acfb2c457e9db898a0951951ff2a3e2add
Mikyo King <mikyo@arize.com>
2024-05-28 19:13:43 -0600

    feat(llamaindex.TS): llama-index-ts initial package creation (#497)
    Co-authored-by: Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>

---
+++ js/packages/openinference-instrumentation-llama-index/src/instrumentation.ts
@@ -0,0 +1,158 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type * as llamaindex from "llamaindex";
+
+import {
+  InstrumentationBase,
+  InstrumentationConfig,
+  InstrumentationModuleDefinition,
+  InstrumentationNodeModuleDefinition,
+  safeExecuteInTheMiddle,
+} from "@opentelemetry/instrumentation";
+import { VERSION } from "./version";
+import {
+  Span,
+  SpanKind,
+  SpanStatusCode,
+  context,
+  diag,
+  trace,
+} from "@opentelemetry/api";
+import { isTracingSuppressed } from "@opentelemetry/core";
+
+const MODULE_NAME = "llamaindex";
+
+/**
+ * Flag to check if the LlamaIndex module has been patched
+ * Note: This is a fallback in case the module is made immutable (e.x. Deno, webpack, etc.)
+ */
+let _isOpenInferencePatched = false;
+
+/**
+ * function to check if instrumentation is enabled / disabled
+ */
+export function isPatched() {
+  return _isOpenInferencePatched;
+}
+
+import {
+  OpenInferenceSpanKind,
+  SemanticConventions,
+} from "@arizeai/openinference-semantic-conventions";
+
+/**
+ * Resolves the execution context for the current span
+ * If tracing is suppressed, the span is dropped and the current context is returned
+ * @param span
+ */
+function getExecContext(span: Span) {
+  const activeContext = context.active();
+  const suppressTracing = isTracingSuppressed(activeContext);
+  const execContext = suppressTracing
+    ? trace.setSpan(context.active(), span)
+    : activeContext;
+  // Drop the span from the context
+  if (suppressTracing) {
+    trace.deleteSpan(activeContext);
+  }
+  return execContext;
+}
+export class LlamaIndexInstrumentation extends InstrumentationBase<
+  typeof llamaindex
+> {
+  constructor(config?: InstrumentationConfig) {
+    super(
+      "@arizeai/openinference-instrumentation-llama-index",
+      VERSION,
+      Object.assign({}, config),
+    );
+  }
+  manuallyInstrument(module: typeof llamaindex) {
+    diag.debug(`Manually instrumenting ${MODULE_NAME}`);
+    this.patch(module);
+  }
+
+  protected init(): InstrumentationModuleDefinition<typeof llamaindex> {
+    const module = new InstrumentationNodeModuleDefinition<typeof llamaindex>(
+      "llamaindex",
+      [">=0.1.0"],
+      this.patch.bind(this),
+      this.unpatch.bind(this),
+    );
+    return module;
+  }
+
+  private patch(moduleExports: typeof llamaindex, moduleVersion?: string) {
+    this._diag.debug(`Applying patch for ${MODULE_NAME}@${moduleVersion}`);
+    if (_isOpenInferencePatched) {
+      return moduleExports;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const instrumentation: LlamaIndexInstrumentation = this;
+
+    type RetrieverQueryEngineQueryType =
+      typeof moduleExports.RetrieverQueryEngine.prototype.query;
+
+    this._wrap(
+      moduleExports.RetrieverQueryEngine.prototype,
+      "query",
+      (original: RetrieverQueryEngineQueryType): any => {
+        return function patchedQuery(
+          this: unknown,
+          ...args: Parameters<RetrieverQueryEngineQueryType>
+        ) {
+          const span = instrumentation.tracer.startSpan(`Query`, {
+            kind: SpanKind.INTERNAL,
+            attributes: {
+              [SemanticConventions.OPENINFERENCE_SPAN_KIND]:
+                OpenInferenceSpanKind.CHAIN,
+              [SemanticConventions.INPUT_VALUE]: args[0].query,
+            },
+          });
+
+          const execContext = getExecContext(span);
+
+          const execPromise = safeExecuteInTheMiddle<
+            ReturnType<RetrieverQueryEngineQueryType>
+          >(
+            () => {
+              return context.with(execContext, () => {
+                return original.apply(this, args);
+              });
+            },
+            (error) => {
+              // Push the error to the span
+              if (error) {
+                span.recordException(error);
+                span.setStatus({
+                  code: SpanStatusCode.ERROR,
+                  message: error.message,
+                });
+                span.end();
+              }
+            },
+          );
+          const wrappedPromise = execPromise.then((result) => {
+            span.setAttributes({
+              [SemanticConventions.OUTPUT_VALUE]: result.response,
+            });
+            span.end();
+            return result;
+          });
+          return context.bind(execContext, wrappedPromise);
+        };
+      },
+    );
+
+    _isOpenInferencePatched = true;
+
+    return moduleExports;
+  }
+
+  private unpatch(moduleExports: typeof llamaindex, moduleVersion?: string) {
+    this._diag.debug(`Un-patching ${MODULE_NAME}@${moduleVersion}`);
+    this._unwrap(moduleExports.RetrieverQueryEngine.prototype, "query");
+
+    _isOpenInferencePatched = false;
+  }
+}
---
+++ js/packages/openinference-instrumentation-llama-index/test/llamaIndex.test.ts
@@ -0,0 +1,118 @@
+import {
+  InMemorySpanExporter,
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-base";
+import { LlamaIndexInstrumentation, isPatched } from "../src/index";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import * as llamaindex from "llamaindex";
+
+const { Document, VectorStoreIndex } = llamaindex;
+
+const DUMMY_RESPONSE = "lorem ipsum";
+
+const tracerProvider = new NodeTracerProvider();
+tracerProvider.register();
+
+const instrumentation = new LlamaIndexInstrumentation();
+instrumentation.disable();
+describe("llamaIndex", () => {
+  it("should pass", () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe("LlamaIndexInstrumentation", () => {
+  const memoryExporter = new InMemorySpanExporter();
+  const spanProcessor = new SimpleSpanProcessor(memoryExporter);
+  instrumentation.setTracerProvider(tracerProvider);
+
+  tracerProvider.addSpanProcessor(spanProcessor);
+  // @ts-expect-error the moduleExports property is private. This is needed to make the test work with auto-mocking
+  instrumentation._modules[0].moduleExports = llamaindex;
+
+  let openAISpy: jest.SpyInstance;
+  let openAITextEmbedSpy: jest.SpyInstance;
+  let openAIQueryEmbedSpy: jest.SpyInstance;
+  beforeAll(() => {
+    instrumentation.enable();
+
+    // Setup the OPENAI_API_KEY
+    process.env["OPENAI_API_KEY"] = "fake-api-key";
+  });
+  afterAll(() => {
+    instrumentation.disable();
+    // Delete the OPENAI_API_KEY
+    delete process.env["OPENAI_API_KEY"];
+  });
+  beforeEach(() => {
+    memoryExporter.reset();
+
+    // Use OpenAI and mock out the calls
+    const response: llamaindex.ChatResponse<llamaindex.ToolCallLLMMessageOptions> =
+      {
+        message: {
+          content: DUMMY_RESPONSE,
+          role: "assistant",
+        },
+        raw: null,
+      };
+    // Mock out the chat completions endpoint
+    openAISpy = jest
+      .spyOn(llamaindex.OpenAI.prototype, "chat")
+      .mockImplementation(() => {
+        return Promise.resolve(response);
+      });
+
+    // Mock out the embeddings response to size 1536 (ada-2)
+    const fakeEmbedding = Array(1536).fill(0);
+    // Mock out the embeddings endpoint
+    openAITextEmbedSpy = jest
+      .spyOn(llamaindex.OpenAIEmbedding.prototype, "getTextEmbeddings")
+      .mockImplementation(() => {
+        return Promise.resolve([fakeEmbedding]);
+      });
+
+    openAIQueryEmbedSpy = jest
+      .spyOn(llamaindex.OpenAIEmbedding.prototype, "getQueryEmbedding")
+      .mockImplementation(() => {
+        return Promise.resolve(fakeEmbedding);
+      });
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+    openAISpy.mockRestore();
+    openAIQueryEmbedSpy.mockRestore();
+    openAITextEmbedSpy.mockRestore();
+  });
+  it("is patched", () => {
+    expect(isPatched()).toBe(true);
+  });
+  it("should create a span for query engines", async () => {
+    // Create Document object with essay
+    const document = new Document({ text: "lorem ipsum" });
+
+    // Split text and create embeddings. Store them in a VectorStoreIndex
+    const index = await VectorStoreIndex.fromDocuments([document]);
+
+    // Query the index
+    const queryEngine = index.asQueryEngine();
+    const response = await queryEngine.query({
+      query: "What did the author do in college?",
+    });
+
+    // Verify that the OpenAI chat method was called once during synthesis
+    expect(openAISpy).toHaveBeenCalledTimes(1);
+    expect(openAIQueryEmbedSpy).toHaveBeenCalledTimes(1);
+    expect(openAITextEmbedSpy).toHaveBeenCalledTimes(1);
+
+    // Output response
+    expect(response.response).toEqual(DUMMY_RESPONSE);
+
+    const spans = memoryExporter.getFinishedSpans();
+    expect(spans.length).toBeGreaterThan(0);
+
+    // Expect a span for the query engine
+    const queryEngineSpan = spans.find((span) => span.name.includes("Query"));
+    expect(queryEngineSpan).toBeDefined();
+  });
+});
