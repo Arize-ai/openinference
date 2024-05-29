787abb3e9411bafa91b927d0b8a7ed8a0cbafdee
Mikyo King <mikyo@arize.com>
2024-05-28 19:13:43 -0600

    feat(llamaindex.TS): llama-index-ts initial package creation (#497)
    Co-authored-by: Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>

---
+++ js/packages/openinference-instrumentation-llama-index/examples/instrumentation.ts
@@ -0,0 +1,37 @@
+import { LlamaIndexInstrumentation } from "../src/index";
+import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base";
+import {
+  NodeTracerProvider,
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-node";
+import { Resource } from "@opentelemetry/resources";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { SEMRESATTRS_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
+import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+// For troubleshooting, set the log level to DiagLogLevel.DEBUG
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
+const provider = new NodeTracerProvider({
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: "llama-index-service",
+  }),
+});
+
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+provider.addSpanProcessor(
+  new SimpleSpanProcessor(
+    new OTLPTraceExporter({
+      url: "http://localhost:6006/v1/traces",
+    }),
+  ),
+);
+
+registerInstrumentations({
+  instrumentations: [new LlamaIndexInstrumentation()],
+});
+
+provider.register();
+
+// eslint-disable-next-line no-console
+console.log("👀 OpenInference initialized");
