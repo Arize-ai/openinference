e274717e04dec5eef7bab5cb059d5d53e1ba4c9a
Roger Yang <roger.yang@arize.com>
2024-06-05 12:16:45 -0700

    update examples

---
+++ python/instrumentation/openinference-instrumentation-langchain/examples/exchange_rate_tool.py
@@ -0,0 +1,48 @@
+from langchain import agents
+from langchain.tools import tool
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from openinference.instrumentation.langchain import LangChainInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk import trace as trace_sdk
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+
+endpoint = "http://127.0.0.1:6006/v1/traces"
+tracer_provider = trace_sdk.TracerProvider()
+tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
+tracer_provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
+
+LangChainInstrumentor().instrument(tracer_provider=tracer_provider)
+
+
+@tool
+def get_exchange_rate(
+    currency_from: str = "USD",
+    currency_to: str = "EUR",
+    currency_date: str = "latest",
+):
+    """Retrieves the exchange rate between two currencies on a specified date."""
+    import requests
+
+    response = requests.get(
+        f"https://api.frankfurter.app/{currency_date}",
+        params={"from": currency_from, "to": currency_to},
+    )
+    return response.json()
+
+
+tools = [get_exchange_rate]
+llm = ChatOpenAI()
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("human", "{input}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+)
+agent = agents.create_tool_calling_agent(llm, tools, prompt)
+agent_executor = agents.AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+if __name__ == "__main__":
+    agent_executor.invoke(
+        {"input": "What is the exchange rate from US dollars to Swedish currency today?"}
+    )
