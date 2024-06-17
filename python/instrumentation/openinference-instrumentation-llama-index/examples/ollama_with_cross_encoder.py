8f2fa1d054bc42996da9825becba682716968bb7
Roger Yang <roger.yang@arize.com>
2024-06-17 13:39:31 -0700

    chore: llama-index example for ollama

--- python/instrumentation/openinference-instrumentation-llama-index/examples/ollama_with_cross_encoder.py
+++ python/instrumentation/openinference-instrumentation-llama-index/examples/ollama_with_cross_encoder.py
@@ -1,10 +1,11 @@
 import tempfile
 from urllib.request import urlretrieve
 
-from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex
-from llama_index.core.response.pprint_utils import pprint_response
-from llama_index.llms.openai import OpenAI
-from llama_index.postprocessor.cohere_rerank import CohereRerank
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.core.postprocessor import SentenceTransformerRerank
+from llama_index.core.settings import Settings
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.llms.ollama import Ollama
 from openinference.instrumentation.llama_index import LlamaIndexInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk import trace as trace_sdk
@@ -16,7 +17,8 @@ tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint
 
 LlamaIndexInstrumentor().instrument(tracer_provider=tracer_provider)
 
-Settings.llm = OpenAI(model="gpt-3.5-turbo")
+Settings.llm = Ollama(model="llama3")
+Settings.embed_model = OllamaEmbedding(model_name="mxbai-embed-large")
 
 essay = "https://raw.githubusercontent.com/run-llama/llama_index/main/docs/docs/examples/data/paul_graham/paul_graham_essay.txt"
 with tempfile.NamedTemporaryFile() as tf:
@@ -24,9 +26,9 @@ with tempfile.NamedTemporaryFile() as tf:
     documents = SimpleDirectoryReader(input_files=[tf.name]).load_data()
 index = VectorStoreIndex.from_documents(documents)
 
-rerank = CohereRerank(top_n=2)
-query_engine = index.as_query_engine(similarity_top_k=10, node_postprocessors=[rerank])
+rerank = SentenceTransformerRerank(model="cross-encoder/ms-marco-MiniLM-L-2-v2", top_n=3)
+chat_engine = index.as_chat_engine(similarity_top_k=10, node_postprocessors=[rerank])
 
 if __name__ == "__main__":
-    response = query_engine.query("What did the author do growing up?")
-    pprint_response(response, show_source=True)
+    response = chat_engine.stream_chat("Which grad schools did the author apply for and why?")
+    response.print_response_stream()
