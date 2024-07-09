ab2bc1553ce85099f8a331e00eb9a2fd10d0cf3e
Roger Yang <roger.yang@arize.com>
2024-07-09 11:45:14 -0700

    fix test

--- python/openinference-instrumentation/tests/test_helpers.py
+++ python/openinference-instrumentation/tests/test_helpers.py
@@ -1,10 +1,14 @@
 from openinference.instrumentation.helpers import get_span_id, get_trace_id
-from opentelemetry.trace import INVALID_SPAN
+from opentelemetry.trace import NonRecordingSpan, SpanContext
 
 
-def test_get_span_id() -> None:
-    assert get_span_id(INVALID_SPAN) == "0000000000000000"
-
-
-def test_get_trace_id() -> None:
-    assert get_trace_id(INVALID_SPAN) == "00000000000000000000000000000000"
+def test_get_span_and_trace_ids() -> None:
+    span = NonRecordingSpan(
+        SpanContext(
+            trace_id=83298525428589810613581548256892812612,
+            span_id=8006530202346048876,
+            is_remote=False,
+        )
+    )
+    assert get_span_id(span) == "6f1ce8cc7245cd6c"
+    assert get_trace_id(span) == "3eaab662c550df264f0fbd19bd8bfd44"
