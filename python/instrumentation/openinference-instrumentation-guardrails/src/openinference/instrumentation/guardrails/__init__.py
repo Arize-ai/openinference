ff409032a3103259c121837c9200bfa8277bcc0e
harrisonchu <harrison@arize.com>
2024-07-15 12:17:26 -0700

    feat(guardrailsai): add instrumentation support (#553)
    Co-authored-by: Roger Yang <80478925+RogerHYang@users.noreply.github.com>

---
+++ python/instrumentation/openinference-instrumentation-guardrails/src/openinference/instrumentation/guardrails/__init__.py
@@ -0,0 +1,116 @@
+import contextvars
+import logging
+from importlib import import_module
+from typing import Any, Collection
+
+from openinference.instrumentation.guardrails._wrap_guard_call import (
+    _ParseCallableWrapper,
+    _PostValidationWrapper,
+    _PromptCallableWrapper,
+)
+from openinference.instrumentation.guardrails.version import __version__
+from opentelemetry import trace as trace_api
+from opentelemetry.instrumentation.instrumentor import BaseInstrumentor  # type: ignore
+from wrapt import ObjectProxy, wrap_function_wrapper
+
+import guardrails as gd
+
+logger = logging.getLogger(__name__)
+
+_instruments = ("guardrails-ai >= 0.4.5",)
+
+_VALIDATION_MODULE = "guardrails.validator_service"
+_LLM_PROVIDERS_MODULE = "guardrails.llm_providers"
+_RUNNER_MODULE = "guardrails.run"
+
+
+class _Contextvars(ObjectProxy):  # type: ignore
+    def __init__(self, cv: Any) -> None:
+        super().__init__(cv)
+
+    @staticmethod
+    def Context() -> contextvars.Context:
+        return contextvars.copy_context()
+
+
+class GuardrailsInstrumentor(BaseInstrumentor):  # type: ignore
+    """An instrumentor for the Guardrails framework."""
+
+    __slots__ = (
+        "_original_guardrails_llm_providers_call",
+        "_original_guardrails_runner_step",
+        "_original_guardrails_validation_after_run",
+    )
+
+    def instrumentation_dependencies(self) -> Collection[str]:
+        return _instruments
+
+    def _instrument(self, **kwargs: Any) -> None:
+        if not (tracer_provider := kwargs.get("tracer_provider")):
+            tracer_provider = trace_api.get_tracer_provider()
+        tracer = trace_api.get_tracer(__name__, __version__, tracer_provider)
+
+        gd.guard.contextvars = _Contextvars(gd.guard.contextvars)
+        gd.async_guard.contextvars = _Contextvars(gd.async_guard.contextvars)
+        for name in ("pydantic", "string", "rail_string", "rail"):
+            wrap_function_wrapper(
+                module="guardrails.guard",
+                name=f"Guard.from_{name}",
+                wrapper=lambda f, _, args, kwargs: f(*args, **{**kwargs, "tracer": tracer}),
+            )
+
+        runner_module = import_module(_RUNNER_MODULE)
+        self._original_guardrails_runner_step = runner_module.Runner.step
+        runner_wrapper = _ParseCallableWrapper(tracer=tracer)
+        wrap_function_wrapper(
+            module=_RUNNER_MODULE,
+            name="Runner.step",
+            wrapper=runner_wrapper,
+        )
+
+        llm_providers_module = import_module(_LLM_PROVIDERS_MODULE)
+        self._original_guardrails_llm_providers_call = (
+            llm_providers_module.PromptCallableBase.__call__
+        )
+        prompt_callable_wrapper = _PromptCallableWrapper(tracer=tracer)
+        wrap_function_wrapper(
+            module=_LLM_PROVIDERS_MODULE,
+            name="PromptCallableBase.__call__",
+            wrapper=prompt_callable_wrapper,
+        )
+
+        validation_module = import_module(_VALIDATION_MODULE)
+        self._original_guardrails_validation_after_run = (
+            validation_module.ValidatorServiceBase.after_run_validator
+        )
+        post_validator_wrapper = _PostValidationWrapper(tracer=tracer)
+        wrap_function_wrapper(
+            module=_VALIDATION_MODULE,
+            name="ValidatorServiceBase.after_run_validator",
+            wrapper=post_validator_wrapper,
+        )
+
+    def _uninstrument(self, **kwargs: Any) -> None:
+        # not unwrapping by checking and using the __wrap__ attribute below because the
+        # original package itself also uses wrapping
+        if self._original_guardrails_llm_providers_call is not None:
+            llm_providers = import_module(_LLM_PROVIDERS_MODULE)
+            llm_providers.PromptCallableBase.__call__ = self._original_guardrails_llm_providers_call
+            self._original_guardrails_llm_providers_call = None
+
+        if self._original_guardrails_runner_step is not None:
+            runner_module = import_module(_RUNNER_MODULE)
+            runner_module.Runner.step = self._original_guardrails_runner_step
+            self._original_guardrails_runner_step = None
+
+        if self._original_guardrails_validation_after_run is not None:
+            validation_module = import_module(_VALIDATION_MODULE)
+            validation_module.ValidatorServiceBase.after_run_validator = (
+                self._original_guardrails_validation_after_run
+            )
+            self._original_guardrails_validation_after_run = None
+
+        if wrapped := getattr(gd.guard.contextvars, "__wrapped__", None):
+            gd.guard.contextvars = wrapped
+        if wrapped := getattr(gd.async_guard.contextvars, "__wrapped__", None):
+            gd.async_guard.contextvars = wrapped
---
+++ python/instrumentation/openinference-instrumentation-guardrails/tests/openinference/instrumentation/guardrails/test_instrumentor.py
@@ -0,0 +1,122 @@
+from typing import Any, Generator
+from unittest.mock import patch
+
+import guardrails
+import pytest
+from guardrails import Guard
+from guardrails.utils.llm_response import LLMResponse  # type: ignore
+from guardrails.validators import TwoWords  # type: ignore
+from openinference.instrumentation.guardrails import GuardrailsInstrumentor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
+
+
+@pytest.fixture()
+def in_memory_span_exporter() -> InMemorySpanExporter:
+    return InMemorySpanExporter()
+
+
+@pytest.fixture()
+def tracer_provider(in_memory_span_exporter: InMemorySpanExporter) -> TracerProvider:
+    resource = Resource(attributes={})
+    tracer_provider = TracerProvider(resource=resource)
+    tracer_provider.add_span_processor(SimpleSpanProcessor(in_memory_span_exporter))
+    return tracer_provider
+
+
+@pytest.fixture()
+def setup_guardrails_instrumentation(
+    tracer_provider: TracerProvider,
+) -> Generator[None, None, None]:
+    GuardrailsInstrumentor().instrument(tracer_provider=tracer_provider)
+    yield
+    GuardrailsInstrumentor().uninstrument()
+
+
+@patch(
+    "guardrails.llm_providers.ArbitraryCallable._invoke_llm",
+    return_value=LLMResponse(output="More Than Two"),
+)
+def test_guardrails_instrumentation(
+    mock_invoke_llm: Any,
+    tracer_provider: TracerProvider,
+    in_memory_span_exporter: InMemorySpanExporter,
+    setup_guardrails_instrumentation: Any,
+) -> None:
+    # we expect the guard to raise an exception here because the mock LLMResponse has more
+    # than two words
+    guard = Guard().use(TwoWords, on_fail="exception")
+    with pytest.raises(Exception):
+        guard(
+            llm_api=lambda prompt: "yoo whatever",
+            prompt="oh look im a bad person",
+        )
+
+    spans = in_memory_span_exporter.get_finished_spans()
+    assert len(spans) >= 3  # Expecting at least 3 spans from the guardrails module
+
+    expected_span_names = {
+        "ArbitraryCallable.__call__",
+        "AsyncValidatorService.after_run_validator",
+        "Runner.step",
+    }
+    found_span_names = set(span.name for span in spans)
+    assert expected_span_names.issubset(found_span_names), "Missing expected spans"
+
+    for span in spans:
+        attributes = dict(span.attributes or dict())
+        if span.name == "ArbitraryCallable.__call__":
+            assert attributes.get("openinference.span.kind") == "LLM"
+            assert span.status.is_ok
+
+        elif span.name == "AsyncValidatorService.after_run_validator":
+            assert attributes.get("validator_name") == "two-words"
+            assert attributes.get("validator_on_fail") == "EXCEPTION"
+            # 3 letter response
+            assert attributes["output.value"] == "fail"
+            # this may be counter intuitive but the exception from the validator actually
+            # occurs in the span for guard_parse, not post_validation
+            assert span.status.is_ok, "post_validation span status should be OK"
+
+        elif span.name == "Runner.step":
+            assert attributes["openinference.span.kind"] == "GUARDRAIL"
+            assert "input.value" in attributes
+            assert not span.status.is_ok, "guard_parse span status should not be OK"
+
+
+def test_guardrails_uninstrumentation(tracer_provider: TracerProvider) -> None:
+    # Store references to the original functions
+    original_prompt_callable_base_call = guardrails.llm_providers.PromptCallableBase.__call__
+    original_runner_step = guardrails.run.Runner.step
+    original_validator_service_base_after_run_validator = (
+        guardrails.validator_service.ValidatorServiceBase.after_run_validator
+    )
+
+    # Instrument the Guardrails to wrap methods
+    GuardrailsInstrumentor().instrument(tracer_provider=tracer_provider)
+
+    # Ensure methods are wrapped
+    assert hasattr(
+        guardrails.llm_providers.PromptCallableBase.__call__, "__wrapped__"
+    ), "Expected PromptCallableBase.__call__ to be wrapped"
+    assert hasattr(guardrails.run.Runner.step, "__wrapped__"), "Expected Runner.step to be wrapped"
+    assert hasattr(
+        guardrails.validator_service.ValidatorServiceBase.after_run_validator, "__wrapped__"
+    ), "Expected ValidatorServiceBase.after_run_validator to be wrapped"
+
+    # Uninstrument the Guardrails to unwrap methods
+    GuardrailsInstrumentor().uninstrument()
+
+    # Ensure methods are unwrapped and point to the original functions
+    assert (
+        guardrails.llm_providers.PromptCallableBase.__call__ is original_prompt_callable_base_call
+    ), "Expected PromptCallableBase.__call__ to be unwrapped"
+    assert (
+        guardrails.run.Runner.step is original_runner_step
+    ), "Expected Runner.step to be unwrapped"
+    assert (
+        guardrails.validator_service.ValidatorServiceBase.after_run_validator
+        is original_validator_service_base_after_run_validator
+    ), "Expected ValidatorServiceBase.after_run_validator to be unwrapped"
