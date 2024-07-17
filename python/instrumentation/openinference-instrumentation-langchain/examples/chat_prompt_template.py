1cf18892c636cb44428e98583d7c8f00be81dc17
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2024-07-17 12:42:55 -0700

    fix(langchain): missing partial variables for chat prompt template (#593)

---
+++ python/instrumentation/openinference-instrumentation-langchain/examples/chat_prompt_template.py
@@ -0,0 +1,19 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from openinference.instrumentation.langchain import LangChainInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk import trace as trace_sdk
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+
+endpoint = "http://127.0.0.1:6006/v1/traces"
+tracer_provider = trace_sdk.TracerProvider()
+tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
+tracer_provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
+
+LangChainInstrumentor().instrument(tracer_provider=tracer_provider)
+
+prompt = ChatPromptTemplate.from_template("{x} {y} {z}?").partial(x="why is", z="blue")
+chain = prompt | ChatOpenAI(model_name="gpt-3.5-turbo")
+
+if __name__ == "__main__":
+    chain.invoke(dict(y="sky"))
