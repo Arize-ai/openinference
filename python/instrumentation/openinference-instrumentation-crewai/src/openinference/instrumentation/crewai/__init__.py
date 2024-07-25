a62e96bfa30ed5ee6d8457ac792daedfd0e956e4
harrison <harrison@arize.com>
2024-07-25 14:23:27 -0400

    add  more attributes

--- python/instrumentation/openinference-instrumentation-crewai/src/openinference/instrumentation/crewai/__init__.py
+++ python/instrumentation/openinference-instrumentation-crewai/src/openinference/instrumentation/crewai/__init__.py
@@ -1,16 +1,87 @@
 import logging
 import json
+from enum import Enum
 from importlib import import_module
-from typing import Any, Callable, Collection, Mapping, Tuple
+from typing import Any, Callable, Collection, Iterator, Mapping, List, Optional, Tuple
+from inspect import signature
+
+from openinference.semconv.trace import OpenInferenceSpanKindValues, SpanAttributes
 
 from opentelemetry import trace as trace_api
 from openinference.instrumentation.crewai.version import __version__
 from opentelemetry.instrumentation.instrumentor import BaseInstrumentor  # type: ignore
+from openinference.instrumentation import get_attributes_from_context, safe_json_dumps
 from wrapt import wrap_function_wrapper
+from opentelemetry.util.types import AttributeValue
 
 _instruments = ("crewai >= 0.41.1",)
 
 logger = logging.getLogger(__name__)
+
+class SafeJSONEncoder(json.JSONEncoder):
+    """
+    Safely encodes non-JSON-serializable objects.
+    """
+
+    def default(self, o: Any) -> Any:
+        try:
+            return super().default(o)
+        except TypeError:
+            if hasattr(o, "dict") and callable(o.dict):  # pydantic v1 models, e.g., from Cohere
+                return o.dict()
+            return repr(o)
+
+def _flatten(mapping: Optional[Mapping[str, Any]]) -> Iterator[Tuple[str, AttributeValue]]:
+    if not mapping:
+        return
+    for key, value in mapping.items():
+        if value is None:
+            continue
+        if isinstance(value, Mapping):
+            for sub_key, sub_value in _flatten(value):
+                yield f"{key}.{sub_key}", sub_value
+        elif isinstance(value, List) and any(isinstance(item, Mapping) for item in value):
+            for index, sub_mapping in enumerate(value):
+                for sub_key, sub_value in _flatten(sub_mapping):
+                    yield f"{key}.{index}.{sub_key}", sub_value
+        else:
+            if isinstance(value, Enum):
+                value = value.value
+            yield key, value
+
+def _get_input_value(method: Callable[..., Any], *args: Any, **kwargs: Any) -> str:
+    """
+    Parses a method call's inputs into a JSON string. Ensures a consistent
+    output regardless of whether the those inputs are passed as positional or
+    keyword arguments.
+    """
+
+    # For typical class methods, the corresponding instance of inspect.Signature
+    # does not include the self parameter. However, the inspect.Signature
+    # instance for __call__ does include the self parameter.
+    method_signature = signature(method)
+    first_parameter_name = next(iter(method_signature.parameters), None)
+    signature_contains_self_parameter = first_parameter_name in ["self"]
+    bound_arguments = method_signature.bind(
+        *(
+            [None]  # the value bound to the method's self argument is discarded below, so pass None
+            if signature_contains_self_parameter
+            else []  # no self parameter, so no need to pass a value
+        ),
+        *args,
+        **kwargs,
+    )
+    return safe_json_dumps(
+        {
+            **{
+                argument_name: argument_value
+                for argument_name, argument_value in bound_arguments.arguments.items()
+                if argument_name not in ["self", "kwargs"]
+            },
+            **bound_arguments.arguments.get("kwargs", {}),
+        },
+        cls=SafeJSONEncoder,
+    )
 
 class _ExecuteCoreWrapper:
     def __init__(self, tracer):
@@ -120,7 +191,7 @@ class _CrewExecution:
         return response
 
 
-class _ToolUse:
+class _ToolUseWrapper:
     def __init__(self, tracer):
         self._tracer = tracer
 
@@ -135,13 +206,27 @@ class _ToolUse:
             span_name = f"{instance.__class__.__name__}.{wrapped.__name__}"
         else:
             span_name = wrapped.__name__
-        with self._tracer.start_as_current_span(span_name) as span:
+        with self._tracer.start_as_current_span(
+            span_name,
+            attributes=dict(
+                _flatten(
+                    {
+                        OPENINFERENCE_SPAN_KIND: OpenInferenceSpanKindValues.TOOL,
+                        SpanAttributes.INPUT_VALUE: _get_input_value(
+                            wrapped,
+                            *args,
+                            **kwargs,
+                        ),
+                    }
+                )
+            ),
+        ) as span:
             tool = kwargs.get("tool")
             tool_name = ""
             if tool:
                 tool_name = tool.name
             span.set_attribute("function_calling_llm", instance.function_calling_llm)
-            span.set_attribute("tool_name", tool_name)
+            span.set_attribute(SpanAttributes.TOOL_NAME, tool_name)
             try:
                 response = wrapped(*args, **kwargs)
             except Exception as exception:
@@ -149,6 +234,7 @@ class _ToolUse:
                 span.record_exception(exception)
                 raise
             span.set_status(trace_api.StatusCode.OK)
+            span.set_attribute(OUTPUT_VALUE, response)
         return response
 
 
@@ -179,7 +265,7 @@ class CrewAIInstrumentor(BaseInstrumentor):  # type: ignore
             wrapper=kickoff_wrapper,
         )
 
-        use_wrapper = _ToolUse(tracer=tracer)
+        use_wrapper = _ToolUseWrapper(tracer=tracer)
         wrap_function_wrapper(
             module="crewai.tools.tool_usage",
             name="ToolUsage._use",
@@ -188,3 +274,7 @@ class CrewAIInstrumentor(BaseInstrumentor):  # type: ignore
 
     def _uninstrument(self, **kwargs: Any) -> None:
         return
+
+INPUT_VALUE = SpanAttributes.INPUT_VALUE
+OPENINFERENCE_SPAN_KIND = SpanAttributes.OPENINFERENCE_SPAN_KIND
+OUTPUT_VALUE = SpanAttributes.OUTPUT_VALUE
