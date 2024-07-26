043636adf393f6581986ceda7e2822625bd55aab
Francisco Castillo <kiko@arize.com>
2024-07-26 15:30:52 -0700

    Revert "clean"
    This reverts commit ac77c6419cd5c864aa50dcf79a022757ced72975.

--- python/openinference-instrumentation/src/openinference/instrumentation/helpers.py
+++
@@ -1,9 +0,0 @@
-from opentelemetry.trace import Span
-
-
-def get_span_id(span: Span) -> str:
-    return span.get_span_context().span_id.to_bytes(8, "big").hex()
-
-
-def get_trace_id(span: Span) -> str:
-    return span.get_span_context().trace_id.to_bytes(16, "big").hex()
