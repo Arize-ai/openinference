863ca2856d373c0be18e566ef0352958670ac8c0
Kiko Castillo <fjcasti1@asu.edu>
2024-07-29 12:14:18 -0700

    feat: Add TracingConfig for configuration settings in instrumentations (#639)

--- python/openinference-instrumentation/src/openinference/instrumentation/helpers.py
+++ python/openinference-instrumentation/src/openinference/instrumentation/helpers.py
@@ -1,3 +1,6 @@
+import json
+from typing import Any
+
 from opentelemetry.trace import Span
 
 
@@ -7,3 +10,12 @@ def get_span_id(span: Span) -> str:
 
 def get_trace_id(span: Span) -> str:
     return span.get_span_context().trace_id.to_bytes(16, "big").hex()
+
+
+def safe_json_dumps(obj: Any, **kwargs: Any) -> str:
+    """
+    A convenience wrapper around `json.dumps` that ensures that any object can
+    be safely encoded without a `TypeError` and that non-ASCII Unicode
+    characters are not escaped.
+    """
+    return json.dumps(obj, default=str, ensure_ascii=False, **kwargs)
