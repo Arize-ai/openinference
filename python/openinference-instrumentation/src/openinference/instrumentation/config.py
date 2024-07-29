fcea7f99e505f104543d3a51a9b3b0f25cba8072
Kiko Castillo <fjcasti1@asu.edu>
2024-07-29 12:14:18 -0700

    feat: Add TracingConfig for configuration settings in instrumentations (#639)

---
+++ python/openinference-instrumentation/src/openinference/instrumentation/config.py
@@ -0,0 +1,216 @@
+import os
+from dataclasses import dataclass, field, fields
+from typing import Any, Optional, get_args
+
+from opentelemetry.context import (
+    _SUPPRESS_INSTRUMENTATION_KEY,
+    attach,
+    detach,
+    set_value,
+)
+
+from .logging import logger
+
+
+class suppress_tracing:
+    """
+    Context manager to pause OpenTelemetry instrumentation.
+
+    Examples:
+        with suppress_tracing():
+            # No tracing will occur within this block
+            ...
+    """
+
+    def __enter__(self) -> "suppress_tracing":
+        self._token = attach(set_value(_SUPPRESS_INSTRUMENTATION_KEY, True))
+        return self
+
+    def __aenter__(self) -> "suppress_tracing":
+        self._token = attach(set_value(_SUPPRESS_INSTRUMENTATION_KEY, True))
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        detach(self._token)
+
+    def __aexit__(self, exc_type, exc_value, traceback) -> None:
+        detach(self._token)
+
+
+OPENINFERENCE_HIDE_INPUTS = "OPENINFERENCE_HIDE_INPUTS"
+# Hides input value & messages
+OPENINFERENCE_HIDE_OUTPUTS = "OPENINFERENCE_HIDE_OUTPUTS"
+# Hides output value & messages
+OPENINFERENCE_HIDE_INPUT_MESSAGES = "OPENINFERENCE_HIDE_INPUT_MESSAGES"
+# Hides all input messages
+OPENINFERENCE_HIDE_OUTPUT_MESSAGES = "OPENINFERENCE_HIDE_OUTPUT_MESSAGES"
+# Hides all output messages
+OPENINFERENCE_HIDE_INPUT_IMAGES = "OPENINFERENCE_HIDE_INPUT_IMAGES"
+# Hides images from input messages
+OPENINFERENCE_HIDE_INPUT_TEXT = "OPENINFERENCE_HIDE_INPUT_TEXT"
+# Hides text from input messages
+OPENINFERENCE_HIDE_OUTPUT_TEXT = "OPENINFERENCE_HIDE_OUTPUT_TEXT"
+# Hides text from output messages
+OPENINFERENCE_HIDE_EMBEDDING_VECTORS = "OPENINFERENCE_HIDE_EMBEDDING_VECTORS"
+# Hides embedding vectors
+OPENINFERENCE_BASE64_IMAGE_MAX_LENGTH = "OPENINFERENCE_BASE64_IMAGE_MAX_LENGTH"
+# Limits characters of a base64 encoding of an image
+REDACTED_VALUE = "__REDACTED__"
+# When a value is hidden, it will be replaced by this redacted value
+
+DEFAULT_HIDE_INPUTS = False
+DEFAULT_HIDE_OUTPUTS = False
+
+DEFAULT_HIDE_INPUT_MESSAGES = False
+DEFAULT_HIDE_OUTPUT_MESSAGES = False
+
+DEFAULT_HIDE_INPUT_IMAGES = False
+DEFAULT_HIDE_INPUT_TEXT = False
+DEFAULT_HIDE_OUTPUT_TEXT = False
+
+DEFAULT_HIDE_EMBEDDING_VECTORS = False
+DEFAULT_BASE64_IMAGE_MAX_LENGTH = 32_000
+
+
+@dataclass(frozen=True)
+class TraceConfig:
+    """
+    TraceConfig helps you modify the observability level of your tracing.
+    For instance, you may want to keep sensitive information from being logged
+    for security reasons, or you may want to limit the size of the base64
+    encoded images to reduce payloads.
+
+    For those attributes not passed, this object tries to read from designated
+    environment variables and, if not found, has default values that maximize
+    observability.
+    """
+
+    hide_inputs: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": OPENINFERENCE_HIDE_INPUTS,
+            "default_value": DEFAULT_HIDE_INPUTS,
+        },
+    )
+    """Hides input value & messages"""
+    hide_outputs: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": OPENINFERENCE_HIDE_OUTPUTS,
+            "default_value": DEFAULT_HIDE_OUTPUTS,
+        },
+    )
+    """Hides output value & messages"""
+    hide_input_messages: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": OPENINFERENCE_HIDE_INPUT_MESSAGES,
+            "default_value": DEFAULT_HIDE_INPUT_MESSAGES,
+        },
+    )
+    """Hides all input messages"""
+    hide_output_messages: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": OPENINFERENCE_HIDE_OUTPUT_MESSAGES,
+            "default_value": DEFAULT_HIDE_OUTPUT_MESSAGES,
+        },
+    )
+    """Hides all output messages"""
+    hide_input_images: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": OPENINFERENCE_HIDE_INPUT_IMAGES,
+            "default_value": DEFAULT_HIDE_INPUT_IMAGES,
+        },
+    )
+    """Hides images from input messages"""
+    hide_input_text: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": OPENINFERENCE_HIDE_INPUT_TEXT,
+            "default_value": DEFAULT_HIDE_INPUT_TEXT,
+        },
+    )
+    """Hides text from input messages"""
+    hide_output_text: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": OPENINFERENCE_HIDE_OUTPUT_TEXT,
+            "default_value": DEFAULT_HIDE_OUTPUT_TEXT,
+        },
+    )
+    """Hides text from output messages"""
+    hide_embedding_vectors: Optional[bool] = field(
+        default=None,
+        metadata={
+            "env_var": OPENINFERENCE_HIDE_EMBEDDING_VECTORS,
+            "default_value": DEFAULT_HIDE_EMBEDDING_VECTORS,
+        },
+    )
+    """Hides embedding vectors"""
+    base64_image_max_length: Optional[int] = field(
+        default=None,
+        metadata={
+            "env_var": OPENINFERENCE_BASE64_IMAGE_MAX_LENGTH,
+            "default_value": DEFAULT_BASE64_IMAGE_MAX_LENGTH,
+        },
+    )
+    """Limits characters of a base64 encoding of an image"""
+
+    def __post_init__(self) -> None:
+        for f in fields(self):
+            expected_type = get_args(f.type)[0]
+            # Optional is Union[T,NoneType]. get_args()returns (T, NoneType).
+            # We collect the first type
+            self._parse_value(
+                f.name,
+                expected_type,
+                f.metadata["env_var"],
+                f.metadata["default_value"],
+            )
+
+    def _parse_value(
+        self,
+        field_name: str,
+        expected_type: Any,
+        env_var: str,
+        default_value: Any,
+    ) -> None:
+        type_name = expected_type.__name__
+        init_value = getattr(self, field_name, None)
+        if init_value is None:
+            env_value = os.getenv(env_var)
+            if env_value is None:
+                object.__setattr__(self, field_name, default_value)
+            else:
+                try:
+                    env_value = self._cast_value(env_value, expected_type)
+                    object.__setattr__(self, field_name, env_value)
+                except Exception:
+                    logger.warning(
+                        f"Could not parse '{env_value}' to {type_name} "
+                        f"for the environment variable '{env_var}'. "
+                        f"Using default value instead: {default_value}."
+                    )
+                    object.__setattr__(self, field_name, default_value)
+        else:
+            if not isinstance(init_value, expected_type):
+                raise TypeError(
+                    f"The field {field_name} must be of type '{type_name}' "
+                    f"but '{type(init_value).__name__}' was found."
+                )
+
+    def _cast_value(
+        self,
+        value: Any,
+        cast_to: Any,
+    ) -> None:
+        if cast_to is bool:
+            if isinstance(value, str) and value.lower() == "true":
+                return True
+            if isinstance(value, str) and value.lower() == "false":
+                return False
+            raise
+        else:
+            return cast_to(value)
