e68bf8cc97faca13a051fcb8b3f3cd5f7bd59ad0
harrison <harrison@arize.com>
2024-07-30 11:56:28 -0400

    unindent

--- python/instrumentation/openinference-instrumentation-crewai/src/openinference/instrumentation/crewai/_wrappers.py
+++ python/instrumentation/openinference-instrumentation-crewai/src/openinference/instrumentation/crewai/_wrappers.py
@@ -150,62 +150,62 @@ class _KickoffWrapper:
             record_exception=False,
             set_status_on_exception=False,
         ) as span:
+            crew = instance
+            inputs = kwargs.get("inputs", None) or (args[0] if args else None)
+
+            span.set_attribute("crew_key", crew.key)
+            span.set_attribute("crew_id", str(crew.id))
+            span.set_attribute("crew_inputs", json.dumps(inputs) if inputs else "")
+            span.set_attribute(
+                "crew_agents",
+                json.dumps(
+                    [
+                        {
+                            "key": agent.key,
+                            "id": str(agent.id),
+                            "role": agent.role,
+                            "goal": agent.goal,
+                            "backstory": agent.backstory,
+                            "verbose?": agent.verbose,
+                            "max_iter": agent.max_iter,
+                            "max_rpm": agent.max_rpm,
+                            "i18n": agent.i18n.prompt_file,
+                            "delegation_enabled": agent.allow_delegation,
+                            "tools_names": [tool.name.casefold() for tool in agent.tools or []],
+                        }
+                        for agent in crew.agents
+                    ]
+                ),
+            )
+            span.set_attribute(
+                "crew_tasks",
+                json.dumps(
+                    [
+                        {
+                            "id": str(task.id),
+                            "description": task.description,
+                            "expected_output": task.expected_output,
+                            "async_execution?": task.async_execution,
+                            "human_input?": task.human_input,
+                            "agent_role": task.agent.role if task.agent else "None",
+                            "agent_key": task.agent.key if task.agent else None,
+                            "context": [task.description for task in task.context]
+                            if task.context
+                            else None,
+                            "tools_names": [tool.name.casefold() for tool in task.tools or []],
+                        }
+                        for task in crew.tasks
+                    ]
+                ),
+            )
             try:
-                crew = instance
-                inputs = kwargs.get("inputs", None) or (args[0] if args else None)
-
-                span.set_attribute("crew_key", crew.key)
-                span.set_attribute("crew_id", str(crew.id))
-                span.set_attribute("crew_inputs", json.dumps(inputs) if inputs else "")
-                span.set_attribute(
-                    "crew_agents",
-                    json.dumps(
-                        [
-                            {
-                                "key": agent.key,
-                                "id": str(agent.id),
-                                "role": agent.role,
-                                "goal": agent.goal,
-                                "backstory": agent.backstory,
-                                "verbose?": agent.verbose,
-                                "max_iter": agent.max_iter,
-                                "max_rpm": agent.max_rpm,
-                                "i18n": agent.i18n.prompt_file,
-                                "delegation_enabled": agent.allow_delegation,
-                                "tools_names": [tool.name.casefold() for tool in agent.tools or []],
-                            }
-                            for agent in crew.agents
-                        ]
-                    ),
-                )
-                span.set_attribute(
-                    "crew_tasks",
-                    json.dumps(
-                        [
-                            {
-                                "id": str(task.id),
-                                "description": task.description,
-                                "expected_output": task.expected_output,
-                                "async_execution?": task.async_execution,
-                                "human_input?": task.human_input,
-                                "agent_role": task.agent.role if task.agent else "None",
-                                "agent_key": task.agent.key if task.agent else None,
-                                "context": [task.description for task in task.context]
-                                if task.context
-                                else None,
-                                "tools_names": [tool.name.casefold() for tool in task.tools or []],
-                            }
-                            for task in crew.tasks
-                        ]
-                    ),
-                )
-
                 response = wrapped(*args, **kwargs)
             except Exception as exception:
                 span.set_status(trace_api.Status(trace_api.StatusCode.ERROR, str(exception)))
                 span.record_exception(exception)
                 raise
             span.set_status(trace_api.StatusCode.OK)
+            span.set_attribute(OUTPUT_VALUE, response)
         return response
 
 
