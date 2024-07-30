b72001b6a514bfa4123d801e12575e018add6298
Shreya Sridhar <ssridhar@arize.com>
2024-07-29 21:50:11 -0700

    Change completion unit tests to use mock_reponse

--- python/instrumentation/openinference-instrumentation-litellm/tests/test_instrumentor.py
+++ python/instrumentation/openinference-instrumentation-litellm/tests/test_instrumentor.py
@@ -41,6 +41,30 @@ class TestLiteLLMInstrumentor:
         litellm.completion(
             model="gpt-3.5-turbo",
             messages=[{"content": "What's the capital of China?", "role": "user"}],
+            mock_response="Beijing",
+        )
+
+        spans = in_memory_span_exporter.get_finished_spans()
+        assert len(spans) == 1
+        span = spans[0]
+        assert span.name == "completion"
+        assert span.attributes[SpanAttributes.LLM_MODEL_NAME] == "gpt-3.5-turbo"
+        assert span.attributes[SpanAttributes.INPUT_VALUE] == "What's the capital of China?"
+
+        assert span.attributes[SpanAttributes.OUTPUT_VALUE] == "Beijing"
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_PROMPT] == 10
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_COMPLETION] == 20
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_TOTAL] == 30
+
+    def test_completion_with_parameters(tracer_provider, in_memory_span_exporter, instrumentor):
+        in_memory_span_exporter.clear()
+
+        litellm.completion(
+            model="gpt-3.5-turbo",
+            messages=[{"content": "What's the capital of China?", "role": "user"}],
+            mock_response="Beijing",
+            temperature=0.7,
+            top_p=0.9,
         )
         spans = in_memory_span_exporter.get_finished_spans()
         assert len(spans) == 1
@@ -48,9 +72,18 @@ class TestLiteLLMInstrumentor:
         assert span.name == "completion"
         assert span.attributes[SpanAttributes.LLM_MODEL_NAME] == "gpt-3.5-turbo"
         assert span.attributes[SpanAttributes.INPUT_VALUE] == "What's the capital of China?"
-        assert SpanAttributes.LLM_INVOCATION_PARAMETERS in span.attributes
+        assert span.attributes[SpanAttributes.LLM_INVOCATION_PARAMETERS] == json.dumps(
+            {"mock_response": "Beijing", "temperature": 0.7, "top_p": 0.9}
+        )
 
-    def test_completion_with_parameters(tracer_provider, in_memory_span_exporter, instrumentor):
+        assert span.attributes[SpanAttributes.OUTPUT_VALUE] == "Beijing"
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_PROMPT] == 10
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_COMPLETION] == 20
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_TOTAL] == 30
+
+    def test_completion_with_multiple_messages(
+        tracer_provider, in_memory_span_exporter, instrumentor
+    ):
         in_memory_span_exporter.clear()
 
         litellm.completion(
@@ -60,7 +93,7 @@ class TestLiteLLMInstrumentor:
                 {"content": "Hello, I can pull up some recipes for cakes.", "role": "assistant"},
                 {"content": "No actually I want to make a pie", "role": "user"},
             ],
-            temperature=0.7,
+            mock_response="Got it! What kind of pie would you like to make?",
         )
         spans = in_memory_span_exporter.get_finished_spans()
         assert len(spans) == 1
@@ -68,10 +101,26 @@ class TestLiteLLMInstrumentor:
         assert span.name == "completion"
         assert span.attributes[SpanAttributes.LLM_MODEL_NAME] == "gpt-3.5-turbo"
         assert span.attributes[SpanAttributes.INPUT_VALUE] == "Hello, I want to bake a cake"
-        assert SpanAttributes.LLM_INVOCATION_PARAMETERS in span.attributes
         assert span.attributes[SpanAttributes.LLM_INVOCATION_PARAMETERS] == json.dumps(
-            {"temperature": 0.7}
+            {"mock_response": "Got it! What kind of pie would you like to make?"}
         )
+        assert span.attributes["input.messages.0.content"] == "Hello, I want to bake a cake"
+        assert (
+            span.attributes["input.messages.1.content"]
+            == "Hello, I can pull up some recipes for cakes."
+        )
+        assert span.attributes["input.messages.2.content"] == "No actually I want to make a pie"
+        assert span.attributes["input.messages.0.role"] == "user"
+        assert span.attributes["input.messages.1.role"] == "assistant"
+        assert span.attributes["input.messages.2.role"] == "user"
+
+        assert (
+            span.attributes[SpanAttributes.OUTPUT_VALUE]
+            == "Got it! What kind of pie would you like to make?"
+        )
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_PROMPT] == 10
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_COMPLETION] == 20
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_TOTAL] == 30
 
     async def test_acompletion(tracer_provider, in_memory_span_exporter, instrumentor):
         in_memory_span_exporter.clear()
@@ -79,6 +128,7 @@ class TestLiteLLMInstrumentor:
         await litellm.acompletion(
             model="gpt-3.5-turbo",
             messages=[{"content": "What's the capital of China?", "role": "user"}],
+            mock_response="Beijing",
         )
 
         spans = in_memory_span_exporter.get_finished_spans()
@@ -87,7 +137,11 @@ class TestLiteLLMInstrumentor:
         assert span.name == "acompletion"
         assert span.attributes[SpanAttributes.LLM_MODEL_NAME] == "gpt-3.5-turbo"
         assert span.attributes[SpanAttributes.INPUT_VALUE] == "What's the capital of China?"
-        assert SpanAttributes.LLM_INVOCATION_PARAMETERS in span.attributes
+
+        assert span.attributes[SpanAttributes.OUTPUT_VALUE] == "Beijing"
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_PROMPT] == 10
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_COMPLETION] == 20
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_TOTAL] == 30
 
     def test_completion_with_retries(tracer_provider, in_memory_span_exporter, instrumentor):
         in_memory_span_exporter.clear()
@@ -95,6 +149,7 @@ class TestLiteLLMInstrumentor:
         litellm.completion_with_retries(
             model="gpt-3.5-turbo",
             messages=[{"content": "What's the capital of China?", "role": "user"}],
+            mock_response="Beijing",
         )
         spans = in_memory_span_exporter.get_finished_spans()
         assert len(spans) == 1
@@ -102,7 +157,11 @@ class TestLiteLLMInstrumentor:
         assert span.name == "completion_with_retries"
         assert span.attributes[SpanAttributes.LLM_MODEL_NAME] == "gpt-3.5-turbo"
         assert span.attributes[SpanAttributes.INPUT_VALUE] == "What's the capital of China?"
-        assert SpanAttributes.LLM_INVOCATION_PARAMETERS in span.attributes
+
+        assert span.attributes[SpanAttributes.OUTPUT_VALUE] == "Beijing"
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_PROMPT] == 10
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_COMPLETION] == 20
+        assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_TOTAL] == 30
 
     # Bug report filed on GitHub for acompletion_with_retries: https://github.com/BerriAI/litellm/issues/4908
     # Until litellm fixes acompletion_with_retries keep this test commented
@@ -117,7 +176,15 @@ class TestLiteLLMInstrumentor:
     #     assert span.name == "acompletion_with_retries"
     #     assert span.attributes[SpanAttributes.LLM_MODEL_NAME] == "gpt-3.5-turbo"
     #     assert span.attributes[SpanAttributes.INPUT_VALUE] == "What's the capital of China?"
-    #     assert SpanAttributes.LLM_INVOCATION_PARAMETERS in span.attributes
+
+    #     assert span.attributes[SpanAttributes.OUTPUT_VALUE] == "Beijing"
+    #     assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_PROMPT] == 10
+    #     assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_COMPLETION] == 20
+    #     assert span.attributes[SpanAttributes.LLM_TOKEN_COUNT_TOTAL] == 30
+
+    # Unlike the completion() functions, liteLLM does not offer a mock_response parameter
+    # for embeddings or image gen yet
+    # For now the following test functions make real calls that require Internet connection
 
     def test_embedding(tracer_provider, in_memory_span_exporter, instrumentor):
         in_memory_span_exporter.clear()
@@ -154,7 +221,11 @@ class TestLiteLLMInstrumentor:
     def test_image_generation(tracer_provider, in_memory_span_exporter, instrumentor):
         in_memory_span_exporter.clear()
 
-        litellm.image_generation(model="dall-e-2", prompt="a sunrise over the mountains")
+        litellm.image_generation(
+            model="dall-e-2",
+            prompt="a sunrise over the mountains",
+            mock_response="mock response url",
+        )
         spans = in_memory_span_exporter.get_finished_spans()
         assert len(spans) == 1
         span = spans[0]
@@ -164,7 +235,11 @@ class TestLiteLLMInstrumentor:
 
     async def test_aimage_generation(tracer_provider, in_memory_span_exporter, instrumentor):
         in_memory_span_exporter.clear()
-        await litellm.aimage_generation(model="dall-e-2", prompt="a sunrise over the mountains")
+        await litellm.aimage_generation(
+            model="dall-e-2",
+            prompt="a sunrise over the mountains",
+            mock_response="mock response url",
+        )
 
         spans = in_memory_span_exporter.get_finished_spans()
         assert len(spans) == 1
@@ -172,3 +247,5 @@ class TestLiteLLMInstrumentor:
         assert span.name == "aimage_generation"
         assert span.attributes[SpanAttributes.LLM_MODEL_NAME] == "dall-e-2"
         assert span.attributes[SpanAttributes.INPUT_VALUE] == "a sunrise over the mountains"
+
+    
