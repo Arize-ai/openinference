a73e79c8aa37cd46bbc0fee33f50c56b89a0e266
Mikyo King <mikyo@arize.com>
2024-07-31 14:51:39 -0600

    chore: add composability tests (#675)

--- js/packages/openinference-core/test/trace/contextAttributes.test.ts
+++ js/packages/openinference-core/test/trace/contextAttributes.test.ts
@@ -8,12 +8,12 @@ import {
 } from "@arizeai/openinference-semantic-conventions";
 
 import {
-  clearSessionId,
+  clearSession,
   getSessionId,
-  setSessionId,
-  setPromptTemplateAttributes,
-  clearPromptTemplateAttributes,
-  getPromptTemplateAttributes,
+  setSession,
+  setPromptTemplate,
+  clearPromptTemplate,
+  getPromptTemplate,
 } from "../../src";
 
 describe("promptTemplate", () => {
@@ -30,13 +30,13 @@ describe("promptTemplate", () => {
       name: "world",
     };
     context.with(
-      setPromptTemplateAttributes(context.active(), {
+      setPromptTemplate(context.active(), {
         template: "hello {name}",
         variables,
         version: "V1.0",
       }),
       () => {
-        expect(getPromptTemplateAttributes(context.active())).toStrictEqual({
+        expect(getPromptTemplate(context.active())).toStrictEqual({
           [PROMPT_TEMPLATE_TEMPLATE]: "hello {name}",
           [PROMPT_TEMPLATE_VARIABLES]: JSON.stringify(variables),
           [PROMPT_TEMPLATE_VERSION]: "V1.0",
@@ -50,19 +50,19 @@ describe("promptTemplate", () => {
       name: "world",
     };
     context.with(
-      setPromptTemplateAttributes(context.active(), {
+      setPromptTemplate(context.active(), {
         template: "hello {name}",
         variables,
         version: "V1.0",
       }),
       () => {
-        expect(getPromptTemplateAttributes(context.active())).toStrictEqual({
+        expect(getPromptTemplate(context.active())).toStrictEqual({
           [PROMPT_TEMPLATE_TEMPLATE]: "hello {name}",
           [PROMPT_TEMPLATE_VARIABLES]: JSON.stringify(variables),
           [PROMPT_TEMPLATE_VERSION]: "V1.0",
         });
-        const ctx = clearPromptTemplateAttributes(context.active());
-        expect(getPromptTemplateAttributes(ctx)).toBeUndefined();
+        const ctx = clearPromptTemplate(context.active());
+        expect(getPromptTemplate(ctx)).toBeUndefined();
       },
     );
   });
@@ -80,7 +80,7 @@ describe("session context attributes", () => {
 
   it("should set session id in the context", () => {
     context.with(
-      setSessionId(context.active(), { sessionId: "session-id" }),
+      setSession(context.active(), { sessionId: "session-id" }),
       async () => {
         expect(getSessionId(context.active())).toBe("session-id");
       },
@@ -89,11 +89,43 @@ describe("session context attributes", () => {
 
   it("should delete session id from the context", () => {
     context.with(
-      setSessionId(context.active(), { sessionId: "session-id" }),
+      setSession(context.active(), { sessionId: "session-id" }),
       async () => {
         expect(getSessionId(context.active())).toBe("session-id");
-        const ctx = clearSessionId(context.active());
+        const ctx = clearSession(context.active());
         expect(getSessionId(ctx)).toBeUndefined();
+      },
+    );
+  });
+});
+
+describe("context.with multiple attributes", () => {
+  let contextManager: ContextManager;
+  beforeEach(() => {
+    contextManager = new AsyncHooksContextManager().enable();
+    context.setGlobalContextManager(contextManager);
+  });
+  afterEach(() => {
+    context.disable();
+  });
+  it("should set multiple attributes on the context", () => {
+    context.with(
+      setSession(
+        setPromptTemplate(context.active(), {
+          template: "hello {name}",
+          variables: { name: "world" },
+          version: "V1.0",
+        }),
+        { sessionId: "session-id" },
+      ),
+
+      async () => {
+        expect(getSessionId(context.active())).toBe("session-id");
+        expect(getPromptTemplate(context.active())).toStrictEqual({
+          [PROMPT_TEMPLATE_TEMPLATE]: "hello {name}",
+          [PROMPT_TEMPLATE_VARIABLES]: JSON.stringify({ name: "world" }),
+          [PROMPT_TEMPLATE_VERSION]: "V1.0",
+        });
       },
     );
   });
