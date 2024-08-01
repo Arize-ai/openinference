8632b297580801896f70feefdcfd37bb6e9eb044
Chris Park <cjpark@berkeley.edu>
2024-08-01 14:41:00 -0700

    feat(js): llama-index-ts initial retrieval span support (#643)

--- js/packages/openinference-instrumentation-llama-index/src/instrumentation.ts
+++ js/packages/openinference-instrumentation-llama-index/src/instrumentation.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import type * as llamaindex from "llamaindex";
 
 import {
@@ -6,18 +5,10 @@ import {
   InstrumentationConfig,
   InstrumentationModuleDefinition,
   InstrumentationNodeModuleDefinition,
-  safeExecuteInTheMiddle,
 } from "@opentelemetry/instrumentation";
+import { diag } from "@opentelemetry/api";
+import { patchQueryMethod, patchRetrieveMethod } from "./utils";
 import { VERSION } from "./version";
-import {
-  Span,
-  SpanKind,
-  SpanStatusCode,
-  context,
-  diag,
-  trace,
-} from "@opentelemetry/api";
-import { isTracingSuppressed } from "@opentelemetry/core";
 
 const MODULE_NAME = "llamaindex";
 
@@ -34,28 +25,6 @@ export function isPatched() {
   return _isOpenInferencePatched;
 }
 
-import {
-  OpenInferenceSpanKind,
-  SemanticConventions,
-} from "@arizeai/openinference-semantic-conventions";
-
-/**
- * Resolves the execution context for the current span
- * If tracing is suppressed, the span is dropped and the current context is returned
- * @param span
- */
-function getExecContext(span: Span) {
-  const activeContext = context.active();
-  const suppressTracing = isTracingSuppressed(activeContext);
-  const execContext = suppressTracing
-    ? trace.setSpan(context.active(), span)
-    : activeContext;
-  // Drop the span from the context
-  if (suppressTracing) {
-    trace.deleteSpan(activeContext);
-  }
-  return execContext;
-}
 export class LlamaIndexInstrumentation extends InstrumentationBase<
   typeof llamaindex
 > {
@@ -66,7 +35,8 @@ export class LlamaIndexInstrumentation extends InstrumentationBase<
       Object.assign({}, config),
     );
   }
-  manuallyInstrument(module: typeof llamaindex) {
+
+  public manuallyInstrument(module: typeof llamaindex) {
     diag.debug(`Manually instrumenting ${MODULE_NAME}`);
     this.patch(module);
   }
@@ -87,71 +57,32 @@ export class LlamaIndexInstrumentation extends InstrumentationBase<
       return moduleExports;
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    const instrumentation: LlamaIndexInstrumentation = this;
-
-    type RetrieverQueryEngineQueryType =
-      typeof moduleExports.RetrieverQueryEngine.prototype.query;
-
+    // TODO: Support streaming
     this._wrap(
       moduleExports.RetrieverQueryEngine.prototype,
       "query",
-      (original: RetrieverQueryEngineQueryType): any => {
-        return function patchedQuery(
-          this: unknown,
-          ...args: Parameters<RetrieverQueryEngineQueryType>
-        ) {
-          const span = instrumentation.tracer.startSpan(`Query`, {
-            kind: SpanKind.INTERNAL,
-            attributes: {
-              [SemanticConventions.OPENINFERENCE_SPAN_KIND]:
-                OpenInferenceSpanKind.CHAIN,
-              [SemanticConventions.INPUT_VALUE]: args[0].query,
-            },
-          });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (original): any => {
+        return patchQueryMethod(original, moduleExports, this.tracer);
+      },
+    );
 
-          const execContext = getExecContext(span);
-
-          const execPromise = safeExecuteInTheMiddle<
-            ReturnType<RetrieverQueryEngineQueryType>
-          >(
-            () => {
-              return context.with(execContext, () => {
-                return original.apply(this, args);
-              });
-            },
-            (error) => {
-              // Push the error to the span
-              if (error) {
-                span.recordException(error);
-                span.setStatus({
-                  code: SpanStatusCode.ERROR,
-                  message: error.message,
-                });
-                span.end();
-              }
-            },
-          );
-          const wrappedPromise = execPromise.then((result) => {
-            span.setAttributes({
-              [SemanticConventions.OUTPUT_VALUE]: result.response,
-            });
-            span.end();
-            return result;
-          });
-          return context.bind(execContext, wrappedPromise);
-        };
+    this._wrap(
+      moduleExports.VectorIndexRetriever.prototype,
+      "retrieve",
+      (original) => {
+        return patchRetrieveMethod(original, moduleExports, this.tracer);
       },
     );
 
     _isOpenInferencePatched = true;
-
     return moduleExports;
   }
 
   private unpatch(moduleExports: typeof llamaindex, moduleVersion?: string) {
     this._diag.debug(`Un-patching ${MODULE_NAME}@${moduleVersion}`);
     this._unwrap(moduleExports.RetrieverQueryEngine.prototype, "query");
+    this._unwrap(moduleExports.VectorIndexRetriever.prototype, "retrieve");
 
     _isOpenInferencePatched = false;
   }
