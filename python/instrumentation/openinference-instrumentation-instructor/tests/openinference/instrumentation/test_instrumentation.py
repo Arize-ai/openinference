012e7e8650a919dd219c11e82024d6f0f7436226
harrison <harrison@arize.com>
2024-08-02 16:16:36 -0400

    add unit test boilerplate

--- python/instrumentation/openinference-instrumentation-instructor/tests/openinference/instrumentation/test_instrumentation.py
+++ python/instrumentation/openinference-instrumentation-instructor/tests/openinference/instrumentation/test_instrumentation.py
@@ -1,35 +1,60 @@
+from typing import Any, Generator
+from unittest.mock import patch
+
 import instructor
+import pytest
 from pydantic import BaseModel
 from openai import OpenAI
-import os
-
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import SimpleSpanProcessor, ConsoleSpanExporter
-
-tracer_provider = TracerProvider()
-tracer_provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
-os.environ["OPENAI_API_KEY"] = "GETYOUROWNDAMNKEY"
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
 from openinference.instrumentation.instructor import InstructorInstrumentor
-from openinference.instrumentation.openai import OpenAIInstrumentor
 
-InstructorInstrumentor().instrument(tracer_provider = tracer_provider)
-OpenAIInstrumentor().instrument(tracer_provider = tracer_provider)
+@pytest.fixture()
+def in_memory_span_exporter() -> InMemorySpanExporter:
+    return InMemorySpanExporter()
+
+@pytest.fixture()
+def tracer_provider(in_memory_span_exporter: InMemorySpanExporter) -> TracerProvider:
+    resource = Resource(attributes={})
+    tracer_provider = TracerProvider(resource=resource)
+    tracer_provider.add_span_processor(SimpleSpanProcessor(in_memory_span_exporter))
+    return tracer_provider
+
+@pytest.fixture()
+def setup_instructor_instrumentation(
+    tracer_provider: TracerProvider,
+) -> Generator[None, None, None]:
+    InstructorInstrumentor().instrument(tracer_provider=tracer_provider)
+    yield
+    InstructorInstrumentor().uninstrument()
 
 class UserInfo(BaseModel):
     name: str
     age: int
 
+def test_instructor_instrumentation(
+    tracer_provider: TracerProvider,
+    in_memory_span_exporter: InMemorySpanExporter,
+    setup_instructor_instrumentation: Any,
+) -> None:
+    # Patch the OpenAI client
+    client = instructor.from_openai(OpenAI())
 
-# Patch the OpenAI client
-client = instructor.from_openai(OpenAI())
+    # Extract structured data from natural language
+    user_info = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        response_model=UserInfo,
+        messages=[{"role": "user", "content": "John Doe is 30 years old."}],
+    )
 
-# Extract structured data from natural language
-user_info = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    response_model=UserInfo,
-    messages=[{"role": "user", "content": "John Doe is 30 years old."}],
-)
+    assert user_info.name == "John Doe"
+    assert user_info.age == 30
 
-print(user_info.name)
-#> John Doe
-print(user_info.age)
+    spans = in_memory_span_exporter.get_finished_spans()
+    assert len(spans) > 0  # Ensure that spans are created
+
+    for span in spans:
+        attributes = dict(span.attributes or dict())
+        assert attributes.get("openinference.span.kind") in ["LLM", "INSTRUCTOR"]
