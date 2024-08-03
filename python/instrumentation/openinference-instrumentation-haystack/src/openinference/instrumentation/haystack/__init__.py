a917b7f21ff637c9a9d24bbe6b1e88d7f100662a
Parth Shisode <pshisode@arize.com>
2024-08-02 17:40:00 -0700

    config + context att + john err

--- python/instrumentation/openinference-instrumentation-haystack/src/openinference/instrumentation/haystack/__init__.py
+++ python/instrumentation/openinference-instrumentation-haystack/src/openinference/instrumentation/haystack/__init__.py
@@ -1,13 +1,13 @@
 import logging
 from typing import Any, Collection
 
+from openinference.instrumentation import OITracer, TraceConfig
 from openinference.instrumentation.haystack._wrappers import _ComponentWrapper, _PipelineWrapper
 from openinference.instrumentation.haystack.version import __version__
 from opentelemetry import trace as trace_api
 from opentelemetry.instrumentation.instrumentor import (  # type: ignore[attr-defined]
     BaseInstrumentor,
 )
-from opentelemetry.trace import get_tracer
 from wrapt import wrap_function_wrapper
 
 import haystack
@@ -20,7 +20,7 @@ _instruments = ("haystack-ai >= 2.0.0",)
 class HaystackInstrumentor(BaseInstrumentor):  # type: ignore[misc]
     """An instrumentor for the Haystack framework."""
 
-    __slots__ = ("_original_pipeline_run", "_original_pipeline_run_component")
+    __slots__ = ("_original_pipeline_run", "_original_pipeline_run_component", "_tracer")
 
     def instrumentation_dependencies(self) -> Collection[str]:
         return _instruments
@@ -28,19 +28,26 @@ class HaystackInstrumentor(BaseInstrumentor):  # type: ignore[misc]
     def _instrument(self, **kwargs: Any) -> None:
         if not (tracer_provider := kwargs.get("tracer_provider")):
             tracer_provider = trace_api.get_tracer_provider()
-        tracer = get_tracer(__name__, __version__, tracer_provider)
+        if not (config := kwargs.get("config")):
+            config = TraceConfig()
+        else:
+            assert isinstance(config, TraceConfig)
+        self._tracer = OITracer(
+            trace_api.get_tracer(__name__, __version__, tracer_provider),
+            config=config,
+        )
 
         self._original_pipeline_run = haystack.Pipeline.run
         wrap_function_wrapper(
             module="haystack.core.pipeline.pipeline",
             name="Pipeline.run",
-            wrapper=_PipelineWrapper(tracer=tracer),
+            wrapper=_PipelineWrapper(tracer=self._tracer),
         )
         self._original_pipeline_run_component = haystack.Pipeline._run_component
         wrap_function_wrapper(
             module="haystack.core.pipeline.pipeline",
             name="Pipeline._run_component",
-            wrapper=_ComponentWrapper(tracer=tracer),
+            wrapper=_ComponentWrapper(tracer=self._tracer),
         )
 
     def _uninstrument(self, **kwargs: Any) -> None:
