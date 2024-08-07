b48b44c9176cbc49d59b73153bd46d0f8a97de48
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2024-08-07 14:07:21 -0700

    chore: fix formatting (#816)

--- python/instrumentation/openinference-instrumentation-haystack/examples/filtering_documents_with_metadata.py
+++ python/instrumentation/openinference-instrumentation-haystack/examples/filtering_documents_with_metadata.py
@@ -17,15 +17,15 @@ HaystackInstrumentor().instrument(tracer_provider=tracer_provider)
 
 documents = [
     Document(
-        content="Use pip to install a basic version of Haystack's latest release: pip install farm-haystack. All the core Haystack components live in the haystack repo. But there's also the haystack-extras repo which contains components that are not as widely used, and you need to install them separately.",
+        content="Use pip to install a basic version of Haystack's latest release: pip install farm-haystack. All the core Haystack components live in the haystack repo. But there's also the haystack-extras repo which contains components that are not as widely used, and you need to install them separately.",  # noqa: E501
         meta={"version": 1.15, "date": datetime(2023, 3, 30)},
     ),
     Document(
-        content="Use pip to install a basic version of Haystack's latest release: pip install farm-haystack[inference]. All the core Haystack components live in the haystack repo. But there's also the haystack-extras repo which contains components that are not as widely used, and you need to install them separately.",
+        content="Use pip to install a basic version of Haystack's latest release: pip install farm-haystack[inference]. All the core Haystack components live in the haystack repo. But there's also the haystack-extras repo which contains components that are not as widely used, and you need to install them separately.",  # noqa: E501
         meta={"version": 1.22, "date": datetime(2023, 11, 7)},
     ),
     Document(
-        content="Use pip to install only the Haystack 2.0 code: pip install haystack-ai. The haystack-ai package is built on the main branch which is an unstable beta version, but it's useful if you want to try the new features as soon as they are merged.",
+        content="Use pip to install only the Haystack 2.0 code: pip install haystack-ai. The haystack-ai package is built on the main branch which is an unstable beta version, but it's useful if you want to try the new features as soon as they are merged.",  # noqa: E501
         meta={"version": 2.0, "date": datetime(2023, 12, 4)},
     ),
 ]
