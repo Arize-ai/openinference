c9a5919df3b10d1e0ace982b8331349dbf888d88
Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>
2024-08-09 16:43:53 -0700

    docs(annotations): add annotations to openai example (#830)

--- js/examples/openai/frontend/app/components/chat-section.tsx
+++ js/examples/openai/frontend/app/components/chat-section.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useChat } from "ai/react";
-import { useMemo } from "react";
-import { insertDataIntoMessages } from "./transform";
+import { useMemo, useState } from "react";
+import { formatMessages } from "./transform";
 import { ChatInput, ChatMessages } from "./ui/chat";
 
 export default function ChatSection() {
@@ -14,22 +14,37 @@ export default function ChatSection() {
     handleInputChange,
     reload,
     stop,
-    data,
   } = useChat({
-    api: process.env.NEXT_PUBLIC_CHAT_API,
+    api: `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/chat`,
     headers: {
       "Content-Type": "application/json", // using JSON because of vercel/ai 2.2.26
     },
   });
 
+  // Map of messageId to feedback score to preserve which messages have had feedback submitted and the score of that feedback
+  const [messageIdToFeedbackMap, setMessageIdToFeedbackMap] = useState<
+    Record<string, number>
+  >({});
+
+  const handleFeedback = (messageId: string, feedbackScore: number) => {
+    setMessageIdToFeedbackMap((prev) => ({
+      ...prev,
+      [messageId]: feedbackScore,
+    }));
+  };
+
   const transformedMessages = useMemo(() => {
-    return insertDataIntoMessages(messages, data);
-  }, [messages, data]);
+    return formatMessages({
+      messages,
+      messageIdToFeedbackMap,
+    });
+  }, [messages, messageIdToFeedbackMap]);
 
   return (
     <div className="space-y-4 max-w-5xl w-full">
       <ChatMessages
         messages={transformedMessages}
+        handleFeedback={handleFeedback}
         isLoading={isLoading}
         reload={reload}
         stop={stop}
