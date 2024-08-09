76539abb146a8ad6465657f6e0b2407f52da484f
Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>
2024-08-09 16:43:53 -0700

    docs(annotations): add annotations to openai example (#830)

---
+++ js/examples/openai/backend/src/controllers/feedback.controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+
+const PHOENIX_API_ENDPOINT =
+  process.env.PHOENIX_API_ENDPOINT || "http://localhost:6006";
+
+const SPAN_ANNOTATIONS_ENDPOINT = `${PHOENIX_API_ENDPOINT}/v1/span_annotations`;
+
+export const feedback = async (req: Request, res: Response) => {
+  const { spanId, feedbackScore } = req.body;
+
+  const response = await fetch(SPAN_ANNOTATIONS_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
+    body: JSON.stringify({
+      data: [
+        {
+          span_id: spanId,
+          annotator_kind: "HUMAN",
+          name: "feedback",
+          result: {
+            label: feedbackScore === 0 ? "👎" : "👍",
+            score: feedbackScore,
+            explanation:
+              feedbackScore === 0
+                ? "Negative feedback from user"
+                : "Positive feedback from user",
+          },
+        },
+      ],
+    }),
+  });
+
+  if (response.status !== 200) {
+    return res.status(500).json({
+      error: "Failed to send feedback",
+    });
+  }
+
+  res.sendStatus(200);
+};
