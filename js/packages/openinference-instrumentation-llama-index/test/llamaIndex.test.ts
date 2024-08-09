5dfa9743e05ce001dd1238a677e2c0a3d5530d70
Chris Park <cjpark@berkeley.edu>
2024-08-08 17:02:07 -0700

    feat(js): llama-index-ts embeddings support (#761)
    Co-authored-by: Parker Stafford <parker.stafford92@gmail.com>
    Co-authored-by: Mikyo King <mikyo@arize.com>

--- js/packages/openinference-instrumentation-llama-index/test/llamaIndex.test.ts
+++ js/packages/openinference-instrumentation-llama-index/test/llamaIndex.test.ts
@@ -1,28 +1,175 @@
+import * as llamaindex from "llamaindex";
+
 import {
   InMemorySpanExporter,
   SimpleSpanProcessor,
 } from "@opentelemetry/sdk-trace-base";
 import { LlamaIndexInstrumentation, isPatched } from "../src/index";
 import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
-import * as llamaindex from "llamaindex";
 import {
   SemanticConventions,
   OpenInferenceSpanKind,
   RETRIEVAL_DOCUMENTS,
 } from "@arizeai/openinference-semantic-conventions";
+import {
+  Document,
+  VectorStoreIndex,
+  GeminiEmbedding,
+  HuggingFaceEmbedding,
+  MistralAIEmbedding,
+  OllamaEmbedding,
+  OpenAIEmbedding,
+  RetrieverQueryEngine,
+} from "llamaindex";
+import { isEmbeddingPrototype, isRetrieverPrototype } from "../src/utils";
+import { OpenAI } from "openai";
 
-const { Document, VectorStoreIndex } = llamaindex;
-
+// Mocked return values
 const DUMMY_RESPONSE = "lorem ipsum";
+const FAKE_EMBEDDING = Array(1536).fill(0); // Mock out the embeddings response to size 1536 (ada-2)
+const RESPONSE_MESSAGE = {
+  content: DUMMY_RESPONSE,
+  role: "assistant",
+};
+
+const EMBEDDING_RESPONSE = {
+  object: "list",
+  data: [
+    { object: "embedding", index: 0, embedding: FAKE_EMBEDDING },
+    { object: "embedding", index: 0, embedding: FAKE_EMBEDDING },
+    { object: "embedding", index: 0, embedding: FAKE_EMBEDDING },
+  ],
+};
+
+// Mock out the chat completions endpoint
+const CHAT_RESPONSE = {
+  id: "chatcmpl-8adq9JloOzNZ9TyuzrKyLpGXexh6p",
+  object: "chat.completion",
+  created: 1703743645,
+  model: "gpt-3.5-turbo-0613",
+  choices: [
+    {
+      index: 0,
+      message: RESPONSE_MESSAGE,
+      logprobs: null,
+      finish_reason: "stop",
+    },
+  ],
+  usage: {
+    prompt_tokens: 12,
+    completion_tokens: 5,
+    total_tokens: 17,
+  },
+};
 
 const tracerProvider = new NodeTracerProvider();
 tracerProvider.register();
 
 const instrumentation = new LlamaIndexInstrumentation();
 instrumentation.disable();
-describe("llamaIndex", () => {
-  it("should pass", () => {
-    expect(true).toBe(true);
+
+/*
+ * Tests for embeddings patching
+ */
+describe("LlamaIndexInstrumentation - Embeddings", () => {
+  const memoryExporter = new InMemorySpanExporter();
+  const spanProcessor = new SimpleSpanProcessor(memoryExporter);
+  instrumentation.setTracerProvider(tracerProvider);
+
+  tracerProvider.addSpanProcessor(spanProcessor);
+  // @ts-expect-error the moduleExports property is private. This is needed to make the test work with auto-mocking
+  instrumentation._modules[0].moduleExports = llamaindex;
+
+  beforeAll(() => {
+    instrumentation.enable();
+    process.env["OPENAI_API_KEY"] = "fake-api-key";
+    process.env["MISTRAL_API_KEY"] = "fake-api-key";
+    process.env["GOOGLE_API_KEY"] = "fake-api-key";
+  });
+
+  afterAll(() => {
+    instrumentation.disable();
+    delete process.env["OPENAI_API_KEY"];
+    delete process.env["MISTRAL_API_KEY"];
+    delete process.env["GOOGLE_API_KEY"];
+  });
+  beforeEach(() => {
+    jest
+      .spyOn(OpenAI.Embeddings.prototype, "create")
+      // @ts-expect-error the response type is not correct - this is just for testing
+      .mockImplementation(async (): Promise<unknown> => {
+        return EMBEDDING_RESPONSE;
+      });
+
+    memoryExporter.reset();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+    jest.resetModules();
+  });
+
+  it("is patched", () => {
+    expect(isPatched()).toBe(true);
+  });
+
+  it("isEmbeddingPrototype should identify retriever prototypes correctly", async () => {
+    // Expect all retriever prototypes to be identified as a retriever
+    expect(isRetrieverPrototype(RetrieverQueryEngine.prototype)).toEqual(true);
+
+    // Expect a non-retriever object to be identified as such
+    expect(isRetrieverPrototype(HuggingFaceEmbedding.prototype)).toEqual(false);
+  });
+
+  it("isEmbeddingPrototype should identify embedder prototypes correctly", async () => {
+    // Expect all embedders to be identified as embeddings
+    expect(isEmbeddingPrototype(HuggingFaceEmbedding.prototype)).toEqual(true);
+    expect(isEmbeddingPrototype(GeminiEmbedding.prototype)).toEqual(true);
+    expect(isEmbeddingPrototype(MistralAIEmbedding.prototype)).toEqual(true);
+    expect(isEmbeddingPrototype(OpenAIEmbedding.prototype)).toEqual(true);
+    expect(isEmbeddingPrototype(OllamaEmbedding.prototype)).toEqual(true);
+
+    // Expect a non-embedding object to be identified as such
+    expect(isEmbeddingPrototype({})).toEqual(false);
+    expect(isEmbeddingPrototype(null)).toEqual(false);
+    expect(isEmbeddingPrototype(undefined)).toEqual(false);
+  });
+
+  it("should create a span for embeddings (query)", async () => {
+    // Get embeddings
+    const embedder = new OpenAIEmbedding();
+    const embeddedVector = await embedder.getQueryEmbedding(
+      "What did the author do in college?",
+    );
+
+    const spans = memoryExporter.getFinishedSpans();
+    expect(spans.length).toBeGreaterThan(0);
+
+    // Expect a span for the embedding
+    const queryEmbeddingSpan = spans.find((span) =>
+      span.name.includes("embedding"),
+    );
+    expect(queryEmbeddingSpan).toBeDefined();
+
+    // Verify span attributes
+    expect(
+      queryEmbeddingSpan?.attributes[
+        SemanticConventions.OPENINFERENCE_SPAN_KIND
+      ],
+    ).toEqual(OpenInferenceSpanKind.EMBEDDING);
+    expect(
+      queryEmbeddingSpan?.attributes[
+        `${SemanticConventions.EMBEDDING_EMBEDDINGS}.0.${SemanticConventions.EMBEDDING_TEXT}`
+      ],
+    ).toEqual("What did the author do in college?");
+    expect(
+      queryEmbeddingSpan?.attributes[
+        `${SemanticConventions.EMBEDDING_EMBEDDINGS}.0.${SemanticConventions.EMBEDDING_VECTOR}`
+      ],
+    ).toEqual(embeddedVector);
+    expect(
+      queryEmbeddingSpan?.attributes[SemanticConventions.EMBEDDING_MODEL_NAME],
+    ).toEqual("text-embedding-ada-002");
   });
 });
 
@@ -35,9 +182,6 @@ describe("LlamaIndexInstrumentation", () => {
   // @ts-expect-error the moduleExports property is private. This is needed to make the test work with auto-mocking
   instrumentation._modules[0].moduleExports = llamaindex;
 
-  let openAISpy: jest.SpyInstance;
-  let openAITextEmbedSpy: jest.SpyInstance;
-  let openAIQueryEmbedSpy: jest.SpyInstance;
   beforeAll(() => {
     instrumentation.enable();
 
@@ -52,46 +196,29 @@ describe("LlamaIndexInstrumentation", () => {
   beforeEach(() => {
     memoryExporter.reset();
 
-    // Use OpenAI and mock out the calls
-    const response: llamaindex.ChatResponse<llamaindex.ToolCallLLMMessageOptions> =
-      {
-        message: {
-          content: DUMMY_RESPONSE,
-          role: "assistant",
-        },
-        raw: null,
-      };
-    // Mock out the chat completions endpoint
-    openAISpy = jest
-      .spyOn(llamaindex.OpenAI.prototype, "chat")
-      .mockImplementation(() => {
-        return Promise.resolve(response);
-      });
-
-    // Mock out the embeddings response to size 1536 (ada-2)
-    const fakeEmbedding = Array(1536).fill(0);
-    // Mock out the embeddings endpoint
-    openAITextEmbedSpy = jest
-      .spyOn(llamaindex.OpenAIEmbedding.prototype, "getTextEmbeddings")
-      .mockImplementation(() => {
-        return Promise.resolve([fakeEmbedding, fakeEmbedding, fakeEmbedding]);
-      });
-
-    openAIQueryEmbedSpy = jest
-      .spyOn(llamaindex.OpenAIEmbedding.prototype, "getQueryEmbedding")
-      .mockImplementation(() => {
-        return Promise.resolve(fakeEmbedding);
-      });
+    jest.spyOn(OpenAI.Chat.Completions.prototype, "create").mockImplementation(
+      // @ts-expect-error the response type is not correct - this is just for testing
+      async (): Promise<unknown> => {
+        return CHAT_RESPONSE;
+      },
+    );
+    jest.spyOn(OpenAI.Embeddings.prototype, "create").mockImplementation(
+      // @ts-expect-error the response type is not correct - this is just for testing
+      async (): Promise<unknown> => {
+        return EMBEDDING_RESPONSE;
+      },
+    );
   });
   afterEach(() => {
     jest.clearAllMocks();
-    openAISpy.mockRestore();
-    openAIQueryEmbedSpy.mockRestore();
-    openAITextEmbedSpy.mockRestore();
+    jest.restoreAllMocks();
+    jest.resetModules();
   });
+
   it("is patched", () => {
     expect(isPatched()).toBe(true);
   });
+
   it("should create a span for query engines", async () => {
     // Create Document object with essay
     const document = new Document({ text: "lorem ipsum" });
@@ -105,13 +232,8 @@ describe("LlamaIndexInstrumentation", () => {
       query: "What did the author do in college?",
     });
 
-    // Verify that the OpenAI chat method was called once during synthesis
-    expect(openAISpy).toHaveBeenCalledTimes(1);
-    expect(openAIQueryEmbedSpy).toHaveBeenCalledTimes(1);
-    expect(openAITextEmbedSpy).toHaveBeenCalledTimes(1);
-
     // Output response
-    expect(response.response).toEqual(DUMMY_RESPONSE);
+    expect(response.response).toEqual(RESPONSE_MESSAGE.content);
 
     const spans = memoryExporter.getFinishedSpans();
     expect(spans.length).toBeGreaterThan(0);
@@ -129,8 +251,9 @@ describe("LlamaIndexInstrumentation", () => {
     ).toEqual("What did the author do in college?");
     expect(
       queryEngineSpan?.attributes[SemanticConventions.OUTPUT_VALUE],
-    ).toEqual(DUMMY_RESPONSE);
+    ).toEqual(RESPONSE_MESSAGE.content);
   });
+
   it("should create a span for retrieve method", async () => {
     // Create Document objects with essays
     const documents = [
@@ -148,11 +271,6 @@ describe("LlamaIndexInstrumentation", () => {
     const response = await retriever.retrieve({
       query: "What did the author do in college?",
     });
-
-    // OpenAI Chat method should not be called
-    expect(openAISpy).toHaveBeenCalledTimes(0);
-    expect(openAIQueryEmbedSpy).toHaveBeenCalledTimes(1);
-    expect(openAITextEmbedSpy).toHaveBeenCalledTimes(1);
 
     const spans = memoryExporter.getFinishedSpans();
     expect(spans.length).toBeGreaterThan(0);
