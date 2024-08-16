ba27c9b3e48d0f9a98444121afb527f561eaab76
shreyabsridhar <121978837+shreyabsridhar@users.noreply.github.com>
2024-08-16 11:57:10 -0700

    Update python/openinference-semantic-conventions/src/openinference/semconv/trace/__init__.py
    Co-authored-by: Mikyo King <mikyo@arize.com>

--- python/openinference-semantic-conventions/src/openinference/semconv/trace/__init__.py
+++ python/openinference-semantic-conventions/src/openinference/semconv/trace/__init__.py
@@ -280,7 +280,7 @@ class ToolAttributes:
 
     TOOL_JSON_SCHEMA = "tool.json_schema"
     """
-    The json schema of the input tools
+    The json schema of a tool input, It is RECOMMENDED that this be in the OpenAI tool calling format: https://platform.openai.com/docs/assistants/tools
     """
 
 
