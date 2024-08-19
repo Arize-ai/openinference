f29eee6b8c28ae398e6c7329d9ca56564c60789d
21ShisodeParth <52021668+21ShisodeParth@users.noreply.github.com>
2024-08-19 16:08:53 -0700

    feat: Anthropic instrumentation (#878)

---
+++ python/instrumentation/openinference-instrumentation-anthropic/examples/sync_completions.py
@@ -0,0 +1,30 @@
+import anthropic
+from anthropic import Anthropic
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk import trace as trace_sdk
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+
+from openinference.instrumentation.anthropic import AnthropicInstrumentor
+
+# Configure HaystackInstrumentor with Phoenix endpoint
+endpoint = "http://127.0.0.1:6006/v1/traces"
+tracer_provider = trace_sdk.TracerProvider()
+tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
+
+AnthropicInstrumentor().instrument(tracer_provider=tracer_provider)
+
+client = Anthropic()
+
+prompt = (
+    f"{anthropic.HUMAN_PROMPT}"
+    f" how does a court case get to the Supreme Court?"
+    f" {anthropic.AI_PROMPT}"
+)
+
+resp = client.completions.create(
+    model="claude-2.1",
+    prompt=prompt,
+    max_tokens_to_sample=1000,
+)
+
+print(resp)
---
+++ python/instrumentation/openinference-instrumentation-anthropic/examples/sync_messages.py
@@ -0,0 +1,27 @@
+from anthropic import Anthropic
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk import trace as trace_sdk
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+
+from openinference.instrumentation.anthropic import AnthropicInstrumentor
+
+# Configure HaystackInstrumentor with Phoenix endpoint
+endpoint = "http://127.0.0.1:6006/v1/traces"
+tracer_provider = trace_sdk.TracerProvider()
+tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
+
+AnthropicInstrumentor().instrument(tracer_provider=tracer_provider)
+
+client = Anthropic()
+
+response = client.messages.create(
+    max_tokens=1024,
+    messages=[
+        {
+            "role": "user",
+            "content": "Hello!",
+        }
+    ],
+    model="claude-3-opus-20240229",
+)
+print(response)
