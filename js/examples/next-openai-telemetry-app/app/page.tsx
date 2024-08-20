d3e793a607d15fe19ea1ea9673fd5b3ea6a3c22f
Parker Stafford <parker.stafford92@gmail.com>
2024-08-20 16:08:23 -0700

    docs(examples): update vercel ai sdk example to have a chat interface with streaming

--- js/examples/next-openai-telemetry-app/app/page.tsx
+++ js/examples/next-openai-telemetry-app/app/page.tsx
@@ -1,51 +1,11 @@
-"use client";
+import Header from "@/app/components/header";
+import ChatSection from "./components/chat-section";
 
-import { useState } from "react";
-
-export default function Page() {
-  const [generation, setGeneration] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
-
+export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
-      <button
-        className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-        onClick={async () => {
-          try {
-            setIsLoading(true);
-
-            const response = await fetch("/api/text", {
-              method: "POST",
-              body: JSON.stringify({
-                prompt: "Why is the sky blue?",
-              }),
-              headers: {
-                "Content-Type": "application/json",
-              },
-            });
-
-            const json = await response.json();
-
-            setGeneration(json.text);
-          } catch (error) {
-            setError(error as Error);
-          } finally {
-            setIsLoading(false);
-          }
-        }}
-      >
-        Generate
-      </button>
-
-      {error && <div className="text-red-500">{error.message}</div>}
-      <div className="mt-4">
-        {isLoading ? (
-          <span className="text-blue-500">Loading...</span>
-        ) : (
-          <span className="text-gray-800">{generation}</span>
-        )}
-      </div>
-    </div>
+    <main className="flex min-h-screen flex-col items-center gap-10 p-24 background-gradient">
+      <Header />
+      <ChatSection />
+    </main>
   );
 }
