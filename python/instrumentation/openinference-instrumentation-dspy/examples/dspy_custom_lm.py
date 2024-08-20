ef809bebf4c2a19cc932b3a828cf6137be73148b
Mikyo King <mikyo@arize.com>
2024-08-20 13:50:06 -0600

    fix(dspy): module resolution for custom LM (#934)

--- python/instrumentation/openinference-instrumentation-dspy/examples/dspy_custom_lm.py
+++ python/instrumentation/openinference-instrumentation-dspy/examples/dspy_custom_lm.py
@@ -1,7 +1,7 @@
 import os
 
 import dspy
-from dsp import LM
+from lm.custom_lm import CustomLM
 from opentelemetry import trace as trace_api
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk import trace as trace_sdk
@@ -9,46 +9,6 @@ from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 from openinference.instrumentation.dspy import DSPyInstrumentor
-
-
-# src: https://dspy-docs.vercel.app/docs/deep-dive/language_model_clients/custom-lm-client
-class CustomLM(LM):
-    """A Fake LM to test instrumentation"""
-
-    def __init__(self, model, api_key, **kwargs):
-        self.model = model
-        self.api_key = api_key
-        self.provider = "default"
-        self.kwargs = {
-            "temperature": 0.0,
-            "max_tokens": 150,
-            "top_p": 1,
-            "frequency_penalty": 0,
-            "presence_penalty": 0,
-            "n": 1,
-            **kwargs,
-        }
-        self.history = []
-
-    def basic_request(self, prompt, **kwargs):
-        response = {"content": [{"text": "This is a test"}]}
-        self.history.append(
-            {
-                "prompt": prompt,
-                "response": "This is a test",
-                "kwargs": kwargs,
-            }
-        )
-
-        return response
-
-    def __call__(self, prompt, only_completed=True, return_sorted=False, **kwargs):
-        response = self.request(prompt, **kwargs)
-
-        completions = [result["text"] for result in response["content"]]
-
-        return completions
-
 
 # Logs to the Phoenix Collector if running locally
 if os.environ.get("PHOENIX_COLLECTOR_ENDPOINT"):
