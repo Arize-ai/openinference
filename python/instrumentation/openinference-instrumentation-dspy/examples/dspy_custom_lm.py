efadd68edb1b66968e7d6f274dc6ea41ce666208
Mikyo King <mikyo@arize.com>
2024-08-19 21:36:03 -0600

    add example

--- python/instrumentation/openinference-instrumentation-dspy/examples/dspy_custom_lm.py
+++ python/instrumentation/openinference-instrumentation-dspy/examples/dspy_custom_lm.py
@@ -1,16 +1,63 @@
 import os
 
 import dspy
-from openinference.instrumentation.dspy import DSPyInstrumentor
+import requests
+from dsp import LM
 from opentelemetry import trace as trace_api
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk import trace as trace_sdk
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
+from openinference.instrumentation.dspy import DSPyInstrumentor
+
+
+class CustomLM(LM):
+    """A Fake LM to test instrumentation"""
+
+    def __init__(self, model, api_key):
+        self.model = model
+        self.api_key = api_key
+        self.provider = "default"
+        self.history = []
+
+    def basic_request(self, prompt, **kwargs):
+        headers = {
+            "x-api-key": self.api_key,
+            "anthropic-version": "2023-06-01",
+            "anthropic-beta": "messages-2023-12-15",
+            "content-type": "application/json",
+        }
+
+        data = {**kwargs, "model": self.model, "messages": [{"role": "user", "content": prompt}]}
+
+        response = requests.post(self.base_url, headers=headers, json=data)
+        response = response.json()
+
+        self.history.append(
+            {
+                "prompt": prompt,
+                "response": response,
+                "kwargs": kwargs,
+            }
+        )
+
+        return response
+
+    def __call__(self, prompt, only_completed=True, return_sorted=False, **kwargs):
+        response = self.request(prompt, **kwargs)
+
+        completions = [result["text"] for result in response["content"]]
+
+        return completions
+
+
 # Logs to the Phoenix Collector if running locally
 if os.environ.get("PHOENIX_COLLECTOR_ENDPOINT"):
     endpoint = os.environ["PHOENIX_COLLECTOR_ENDPOINT"] + "/v1/traces"
+else:
+    # Assume a local collector
+    endpoint = "http://localhost:6006/v1/traces"
 
 resource = Resource(attributes={})
 tracer_provider = trace_sdk.TracerProvider(resource=resource)
@@ -33,7 +80,7 @@ class BasicQA(dspy.Signature):
 if __name__ == "__main__":
     turbo = dspy.OpenAI(model="gpt-3.5-turbo")
 
-    dspy.settings.configure(lm=turbo)
+    dspy.settings.configure(lm=CustomLM("fake-model", "fake-api-key"))
 
     # Define the predictor.
     generate_answer = dspy.Predict(BasicQA)
