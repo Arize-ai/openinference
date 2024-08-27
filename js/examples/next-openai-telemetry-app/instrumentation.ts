8401b67e087a1b09dae22758f1672a76ee80ae6e
Mikyo King <mikyo@arize.com>
2024-08-27 16:34:38 -0600

    ci: Deploy setup for vercel telemetry (#978)

--- js/examples/next-openai-telemetry-app/instrumentation.ts
+++ js/examples/next-openai-telemetry-app/instrumentation.ts
@@ -1,20 +1,29 @@
 import { registerOTel } from "@vercel/otel";
 import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
 import { OpenInferenceSpanProcessor } from "@arizeai/openinference-vercel";
-import { OTLPHttpProtoTraceExporter } from "@vercel/otel";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { SEMRESATTRS_PROJECT_NAME } from "@arizeai/openinference-semantic-conventions";
 
 // For troubleshooting, set the log level to DiagLogLevel.DEBUG
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 export function register() {
   registerOTel({
-    serviceName: "next-app",
+    serviceName: "phoenix-next-app",
+    attributes: {
+      [SEMRESATTRS_PROJECT_NAME]: "phoenix-next-app",
+    },
     spanProcessors: [
       new OpenInferenceSpanProcessor(),
       new SimpleSpanProcessor(
-        new OTLPHttpProtoTraceExporter({
-          url: "http://localhost:6006/v1/traces",
+        new OTLPTraceExporter({
+          headers: {
+            api_key: process.env["PHOENIX_API_KEY"],
+          },
+          url:
+            process.env["PHOENIX_COLLECTOR_ENDPOINT"] ||
+            "https://app.phoenix.arize.com/v1/traces",
         }),
       ),
     ],
