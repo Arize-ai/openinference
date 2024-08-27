e293624d33ed6250fc26049b6c53e727382089b6
Mikyo King <mikyo@arize.com>
2024-08-27 16:08:01 -0600

    change environment variables

--- js/examples/next-openai-telemetry-app/instrumentation.ts
+++ js/examples/next-openai-telemetry-app/instrumentation.ts
@@ -1,11 +1,14 @@
 import { registerOTel } from "@vercel/otel";
 import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
 import { OpenInferenceSpanProcessor } from "@arizeai/openinference-vercel";
-import { OTLPHttpProtoTraceExporter } from "@vercel/otel";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import assert from "assert";
 
 // For troubleshooting, set the log level to DiagLogLevel.DEBUG
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
+assert(process.env["PHOENIX_API_KEY"], "PHOENIX_API_KEY is required");
 
 export function register() {
   registerOTel({
@@ -13,8 +16,11 @@ export function register() {
     spanProcessors: [
       new OpenInferenceSpanProcessor(),
       new SimpleSpanProcessor(
-        new OTLPHttpProtoTraceExporter({
-          url: "http://localhost:6006/v1/traces",
+        new OTLPTraceExporter({
+          headers: {
+            api_key: process.env["PHOENIX_API_KEY"],
+          },
+          url: "https://app.phoenix.arize.com/v1/traces",
         }),
       ),
     ],
