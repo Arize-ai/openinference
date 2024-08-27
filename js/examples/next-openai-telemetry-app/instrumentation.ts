1c80352727e96557266190de206b05024fe17ea4
Mikyo King <mikyo@arize.com>
2024-08-27 16:13:24 -0600

    add cspell

--- js/examples/next-openai-telemetry-app/instrumentation.ts
+++ js/examples/next-openai-telemetry-app/instrumentation.ts
@@ -3,16 +3,17 @@ import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
 import { OpenInferenceSpanProcessor } from "@arizeai/openinference-vercel";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
-import assert from "assert";
+import { SEMRESATTRS_PROJECT_NAME } from "@arizeai/openinference-semantic-conventions";
 
 // For troubleshooting, set the log level to DiagLogLevel.DEBUG
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
-assert(process.env["PHOENIX_API_KEY"], "PHOENIX_API_KEY is required");
-
 export function register() {
   registerOTel({
-    serviceName: "next-app",
+    serviceName: "phoenix-next-app",
+    attributes: {
+      [SEMRESATTRS_PROJECT_NAME]: "phoenix-next-app",
+    },
     spanProcessors: [
       new OpenInferenceSpanProcessor(),
       new SimpleSpanProcessor(
