a556a1a443694defb2fd970ebdbc5afe97199636
Alexander Song <axiomofjoy@gmail.com>
2024-09-05 14:29:48 -0700

    fix

--- python/instrumentation/openinference-instrumentation-llama-index/examples/openai_stream_chat.py
+++ python/instrumentation/openinference-instrumentation-llama-index/examples/openai_stream_chat.py
@@ -1,5 +1,13 @@
-from llama_index.core.base.llms.types import ChatMessage
-from llama_index.llms.openai import OpenAI
+"""
+This script requires that your Neutrino API key is set in the NEUTRINO_API_KEY
+environment variable and that some text files are placed in the `files`
+directory.
+
+https://www.neutrinoapp.com/
+"""
+
+from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex
+from llama_index.llms.neutrino import Neutrino
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk import trace as trace_sdk
 from opentelemetry.sdk.trace.export import SimpleSpanProcessor
@@ -12,12 +20,11 @@ tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint
 
 LlamaIndexInstrumentor().instrument(tracer_provider=tracer_provider)
 
-llm = OpenAI(model="gpt-3.5-turbo")
-
-if __name__ == "__main__":
-    response_gen = llm.stream_chat(
-        [ChatMessage(content="hello")],
-        stream_options={"include_usage": True},
-    )
-    for response in response_gen:
-        print(response.delta, end="")
+Settings.llm = Neutrino(
+    model="chat-preview",
+    api_base="https://router.neutrinoapp.com/api/engines",
+)
+documents = SimpleDirectoryReader("files").load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+response = query_engine.query("summarize each document in a few sentences")
