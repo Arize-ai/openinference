a06f27bad8fa414051de195cafc561df10b7fba4
Roger Yang <roger.yang@arize.com>
2024-09-10 09:05:57 -0700

    clean up

--- python/openinference-instrumentation/tests/test_id_generator.py
+++ python/openinference-instrumentation/tests/test_id_generator.py
@@ -11,12 +11,15 @@ def test_id_generator_is_unaffected_by_seed() -> None:
     in_memory_span_exporter = InMemorySpanExporter()
     tracer_provider = TracerProvider()
     tracer_provider.add_span_processor(SimpleSpanProcessor(in_memory_span_exporter))
-    tracer = OITracer(tracer_provider.get_tracer(__name__), TraceConfig())
+    tracer = tracer_provider.get_tracer(__name__)
+    oi_tracer = OITracer(tracer, TraceConfig())
     n = 10
-    for _ in range(n):
-        seed(42)
-        tracer.start_span("test").end()
+    for tr in (tracer, oi_tracer):
+        for _ in range(n):
+            seed(42)
+            with tr.start_as_current_span("parent"):
+                tr.start_span("child").end()
     spans = in_memory_span_exporter.get_finished_spans()
-    assert len(spans) == n
-    assert len(set(span.context.trace_id for span in spans)) == n
-    assert len(set(span.context.span_id for span in spans)) == n
+    assert len(spans) == n * 2 * 2
+    assert len(set(span.context.trace_id for span in spans)) == (n + 1)
+    assert len(set(span.context.span_id for span in spans)) == (n + 1) * 2
