c1e0c11fe44f46b5fefa7895abeb9d08ca29689a
Alexander Song <axiomofjoy@gmail.com>
2024-10-09 13:54:03 -0700

    lm example

---
+++ python/instrumentation/openinference-instrumentation-dspy/examples/lm.py
@@ -0,0 +1,19 @@
+from dspy import LM
+from opentelemetry import trace as trace_api
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk import trace as trace_sdk
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+
+from openinference.instrumentation.dspy import DSPyInstrumentor
+
+endpoint = "http://localhost:6006/v1/traces"
+tracer_provider = trace_sdk.TracerProvider()
+tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
+tracer_provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
+trace_api.set_tracer_provider(tracer_provider=tracer_provider)
+DSPyInstrumentor().instrument(tracer_provider=tracer_provider)
+
+
+lm = LM("openai/gpt-4")
+output = lm("hello, how are you?")
+print(f"{output=}")
