19d6f7ebab22f6bab36f4d14f72561bcbf062be0
Alexander Song <axiomofjoy@gmail.com>
2024-10-10 13:34:27 -0700

    correct example

--- python/instrumentation/openinference-instrumentation-dspy/examples/lm.py
+++ python/instrumentation/openinference-instrumentation-dspy/examples/lm.py
@@ -1,5 +1,4 @@
 from dspy import LM
-from opentelemetry import trace as trace_api
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk import trace as trace_sdk
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
@@ -10,7 +9,6 @@ endpoint = "http://localhost:6006/v1/traces"
 tracer_provider = trace_sdk.TracerProvider()
 tracer_provider.add_span_processor(SimpleSpanProcessor(OTLPSpanExporter(endpoint)))
 tracer_provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
-trace_api.set_tracer_provider(tracer_provider=tracer_provider)
 DSPyInstrumentor().instrument(tracer_provider=tracer_provider)
 
 
