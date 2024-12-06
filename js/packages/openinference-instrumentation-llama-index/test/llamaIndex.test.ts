ca82d05269c316d92410e7c60ac1b77d62584b27
Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>
2024-12-06 14:46:52 -0800

    chore: remove llamaindex npm package (#1156)

--- js/packages/openinference-instrumentation-llama-index/test/llamaIndex.test.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Dummy test for scaffolding package, remove when real tests are added
- */
-describe("llamaIndex", () => {
-  it("should pass", () => {
-    expect(true).toBe(true);
-  });
-});
