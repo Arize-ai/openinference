b46931c7cec73f73873a3ae15003f3c6c52173e1
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2024-12-12 08:27:09 -0800

    feat: add get_current_span helper function for llama-index (#1165)

---
+++ python/instrumentation/openinference-instrumentation-llama-index/tests/openinference/instrumentation/llama_index/test_get_current_span.py
@@ -0,0 +1,51 @@
+from asyncio import create_task, gather, sleep
+from random import random
+from typing import Iterator
+
+import pytest
+from llama_index.core.instrumentation import get_dispatcher
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
+
+from openinference.instrumentation.llama_index import LlamaIndexInstrumentor, get_current_span
+from openinference.semconv.trace import SpanAttributes
+
+dispatcher = get_dispatcher(__name__)
+
+
+@dispatcher.span  # type: ignore[misc,unused-ignore]
+async def foo(k: int) -> str:
+    child = create_task(foo(k - 1)) if k > 1 else None
+    await sleep(random() / 100)
+    span = get_current_span()
+    if child:
+        await child
+    return str(span.get_span_context().span_id) if span else ""
+
+
+async def test_get_current_span(
+    in_memory_span_exporter: InMemorySpanExporter,
+) -> None:
+    n, k = 10, 5
+    await gather(*(foo(k) for _ in range(n)))
+    spans = in_memory_span_exporter.get_finished_spans()
+    assert len(spans) == n * k
+    seen = set()
+    for span in spans:
+        assert span.attributes and span.context
+        assert (expected := str(span.context.span_id)) not in seen
+        seen.add(expected)
+        assert span.attributes.get(OUTPUT_VALUE) == expected
+
+
+@pytest.fixture(autouse=True)
+def instrument(
+    tracer_provider: TracerProvider,
+    in_memory_span_exporter: InMemorySpanExporter,
+) -> Iterator[None]:
+    LlamaIndexInstrumentor().instrument(tracer_provider=tracer_provider)
+    yield
+    LlamaIndexInstrumentor().uninstrument()
+
+
+OUTPUT_VALUE = SpanAttributes.OUTPUT_VALUE
