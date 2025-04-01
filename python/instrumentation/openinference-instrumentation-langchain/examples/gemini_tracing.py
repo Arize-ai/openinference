eb826c026d7ce70d3b0dc84200b9a492058a6504
JohnGilhuly <johnrgilhuly@gmail.com>
2025-04-01 17:04:21 -0400

    style fix

--- python/instrumentation/openinference-instrumentation-langchain/examples/gemini_tracing.py
+++ python/instrumentation/openinference-instrumentation-langchain/examples/gemini_tracing.py
@@ -9,7 +9,7 @@ os.environ["PHOENIX_CLIENT_HEADERS"] = "api_key="
 os.environ["PHOENIX_COLLECTOR_ENDPOINT"] = "https://app.phoenix.arize.com"
 
 # Register Phoenix Tracer
-tracer_provider = register(project_name='gemini-responses-demo', auto_instrument=True)
+tracer_provider = register(project_name="gemini-responses-demo", auto_instrument=True)
 tracer = tracer_provider.get_tracer(__name__)
 
 # Streaming Call
@@ -17,5 +17,5 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", google_api_key=os.environ
 result = llm.stream("What are the usecases of LLMs?")
 for x in result:
     print(x.content, end="", flush=True)
-    
+
 llm.invoke("What are the usecases of LLMs?")
