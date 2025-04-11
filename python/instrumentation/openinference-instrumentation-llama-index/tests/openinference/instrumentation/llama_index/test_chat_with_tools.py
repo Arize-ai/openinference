df901b7b17cfba781648e9d568f5f0b56f967826
Roger Yang <roger.yang@arize.com>
2025-04-11 08:02:17 -0700

    clean up

--- python/instrumentation/openinference-instrumentation-llama-index/tests/openinference/instrumentation/llama_index/test_chat_with_tools.py
+++ python/instrumentation/openinference-instrumentation-llama-index/tests/openinference/instrumentation/llama_index/test_chat_with_tools.py
@@ -6,22 +6,27 @@ import pytest
 from llama_index.core.llms.function_calling import FunctionCallingLLM
 from llama_index.core.tools import FunctionTool
 from llama_index.llms.anthropic import Anthropic
-from llama_index.llms.openai import OpenAI, OpenAIResponses
+from llama_index.llms.openai import OpenAI
 from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
 from opentelemetry.trace import TracerProvider
 
 from openinference.instrumentation.llama_index import LlamaIndexInstrumentor
 
+llms = [
+    OpenAI(max_retries=0),
+    Anthropic(max_retries=0),
+]
+
+try:
+    from llama_index.llms.openai import OpenAIResponses
+
+    llms.append(OpenAIResponses(max_retries=0))
+except Exception:
+    pass
+
 
 @pytest.mark.disable_socket
-@pytest.mark.parametrize(
-    "llm",
-    [
-        OpenAI(max_retries=0),
-        OpenAIResponses(max_retries=0),
-        Anthropic(max_retries=0),
-    ],
-)
+@pytest.mark.parametrize("llm", llms)
 def test_openai_chat_with_tools(
     llm: FunctionCallingLLM,
     in_memory_span_exporter: InMemorySpanExporter,
@@ -32,7 +37,7 @@ def test_openai_chat_with_tools(
         llm.chat_with_tools(tools, "")
     spans = in_memory_span_exporter.get_finished_spans()
     assert spans
-    span = next(s for s in spans if s.name.endswith(".chat"))
+    span = next(s for s in spans if s.name.endswith("chat"))
     attributes = dict(span.attributes or {})
     for i in range(len(tools)):
         assert attributes[f"llm.tools.{i}.tool.json_schema"]
