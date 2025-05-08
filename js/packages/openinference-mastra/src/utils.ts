9ba107e016693bbfc027bde1b65d1053bae3c383
Tony Powell <apowell@arize.com>
2025-05-08 14:58:41 -0400

    Restructure exports

--- js/packages/openinference-mastra/src/utils.ts
+++ js/packages/openinference-mastra/src/utils.ts
@@ -1,7 +1,15 @@
-import { SEMRESATTRS_PROJECT_NAME } from "@arizeai/openinference-semantic-conventions";
 import type { ReadableSpan } from "@opentelemetry/sdk-trace-base";
+import { SEMRESATTRS_PROJECT_NAME } from "@arizeai/openinference-semantic-conventions";
+import { isOpenInferenceSpan as isOpenInferenceSpanVercel } from "@arizeai/openinference-vercel/utils";
 import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
 
+/**
+ * Augments a Mastra span with OpenInference resource attributes.
+ *
+ * This function will add additional attributes to the span, based on the Mastra span's resource attributes.
+ *
+ * @param span - The Mastra span to augment.
+ */
 export const addOpenInferenceResourceAttributesToMastraSpan = (
   span: ReadableSpan,
 ) => {
@@ -9,8 +17,6 @@ export const addOpenInferenceResourceAttributesToMastraSpan = (
   if (ATTR_SERVICE_NAME in attributes) {
     attributes[SEMRESATTRS_PROJECT_NAME] = attributes[ATTR_SERVICE_NAME];
   }
-  // eslint-disable-next-line no-console
-  console.log("attributes", attributes);
 };
 
 /**
@@ -24,3 +30,17 @@ export const addOpenInferenceAttributesToMastraSpan = (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   span: ReadableSpan,
 ) => {};
+
+/**
+ * Checks if a span is an OpenInference span.
+ *
+ * This function will check if the span is an OpenInference annotated span.
+ *
+ * It can be used as a span filter for the OpenInferenceTraceExporter, to ensure that only OpenInference annotated spans are exported.
+ *
+ * @param span - The span to check.
+ */
+export const isOpenInferenceSpan = (span: ReadableSpan) => {
+  // TODO: Implement Mastra span check in addition to Vercel span check
+  return isOpenInferenceSpanVercel(span);
+};
