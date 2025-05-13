56b104aaf5f19b9037cb3614e8b8753c2e423fc4
daavoo <daviddelaiglesiacastro@gmail.com>
2025-05-13 12:14:10 +0200

    fix(agno): Allow agent.name to be `None`.
    Passing no name was resulting in `AttributeError: 'NoneType' object has no attribute 'replace'`.

--- python/instrumentation/openinference-instrumentation-agno/src/openinference/instrumentation/agno/_wrappers.py
+++ python/instrumentation/openinference-instrumentation-agno/src/openinference/instrumentation/agno/_wrappers.py
@@ -187,7 +187,7 @@ class _RunWrapper:
                 yield response
 
         agent = instance
-        if hasattr(agent, "name"):
+        if hasattr(agent, "name") and agent.name:
             agent_name = agent.name.replace(" ", "_").replace("-", "_")
         else:
             agent_name = "Agent"
