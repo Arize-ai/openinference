36d72565df684be074234c8ee0f997b9e5c8d536
David Monical <davidgmonical@berkeley.edu>
2025-05-16 10:49:19 -0700

    change package name

---
+++ python/instrumentation/openinference-instrumentation-pydanticai/src/openinference/instrumentation/pydanticai/package.py
@@ -0,0 +1,2 @@
+_instruments = ("pydantic >= 2.10.0",)
+_supports_metrics = False
---
+++ python/instrumentation/openinference-instrumentation-pydanticai/src/openinference/instrumentation/pydanticai/span_exporter.py
@@ -0,0 +1,55 @@
+from typing import Optional, Sequence
+
+from opentelemetry.sdk.trace import ReadableSpan
+from opentelemetry.sdk.trace.export import SpanExporter, SpanExportResult
+
+from openinference.instrumentation.pydanticai.semantic_conventions import (
+    OpenInferenceAttributesExtractor,
+)
+from openinference.instrumentation.pydanticai.utils import SpanFilter, should_export_span
+
+
+class OpenInferenceSpanExporter(SpanExporter):
+    def __init__(self, base_exporter, span_filter: Optional[SpanFilter] = None):
+        self._base_exporter = base_exporter
+        self._extractor = OpenInferenceAttributesExtractor()
+        self._span_filter = span_filter
+
+    def export(self, spans: Sequence[ReadableSpan]) -> SpanExportResult:
+        openinference_spans = []
+
+        for span in spans:
+            openinference_attributes = {}
+
+            if span.attributes is not None:
+                openinference_attributes_iter = self._extractor.get_attributes(span.attributes)
+                openinference_attributes = dict(openinference_attributes_iter)
+
+            openinference_span = ReadableSpan(
+                name=span.name,
+                context=span.context,
+                parent=span.parent,
+                resource=span.resource,
+                attributes=openinference_attributes,
+                events=span.events,
+                links=span.links,
+                kind=span.kind,
+                status=span.status,
+                start_time=span.start_time,
+                end_time=span.end_time,
+                instrumentation_scope=span.instrumentation_scope,
+            )
+
+            if not should_export_span(openinference_span, self._span_filter):
+                continue
+
+            openinference_spans.append(openinference_span)
+
+        # Export the openinference spans
+        return self._base_exporter.export(openinference_spans)
+
+    def shutdown(self):
+        return self._base_exporter.shutdown()
+
+    def force_flush(self, timeout_millis: int = 30000) -> bool:
+        return self._base_exporter.force_flush(timeout_millis)
