ad02b1925f873567f56152069778595edb6d8ad5
Dirk Brand <dirkbrnd@gmail.com>
2025-05-16 15:31:24 +0200

    Update for new Agno structure

--- python/instrumentation/openinference-instrumentation-agno/src/openinference/instrumentation/agno/_wrappers.py
+++ python/instrumentation/openinference-instrumentation-agno/src/openinference/instrumentation/agno/_wrappers.py
@@ -118,6 +118,50 @@ class _RunWrapper:
     ) -> Any:
         if context_api.get_value(context_api._SUPPRESS_INSTRUMENTATION_KEY):
             return wrapped(*args, **kwargs)
+        agent = instance
+        if hasattr(agent, "name") and agent.name:
+            agent_name = agent.name.replace(" ", "_").replace("-", "_")
+        else:
+            agent_name = "Agent"
+        span_name = f"{agent_name}.run"
+
+        with self._tracer.start_as_current_span(
+            span_name,
+            attributes=dict(
+                _flatten(
+                    {
+                        OPENINFERENCE_SPAN_KIND: AGENT,
+                        INPUT_VALUE: _get_input_value(
+                            wrapped,
+                            *args,
+                            **kwargs,
+                        ),
+                        **dict(_agent_run_attributes(agent)),
+                        **dict(get_attributes_from_context()),
+                    }
+                )
+            ),
+        ) as span:
+            try:
+                run_response = wrapped(*args, **kwargs)
+                
+                span.set_status(trace_api.StatusCode.OK)
+                span.set_attribute(OUTPUT_VALUE, run_response.to_json())
+                return run_response
+
+            except Exception as e:
+                span.set_status(trace_api.StatusCode.ERROR, str(e))
+                raise
+
+    def run_stream(
+        self,
+        wrapped: Callable[..., Any],
+        instance: Any,
+        args: Tuple[Any, ...],
+        kwargs: Mapping[str, Any],
+    ) -> Any:
+        if context_api.get_value(context_api._SUPPRESS_INSTRUMENTATION_KEY):
+            return wrapped(*args, **kwargs)
 
         agent = instance
         if hasattr(agent, "name") and agent.name:
@@ -144,28 +188,10 @@ class _RunWrapper:
             ),
         ) as span:
             try:
-                if "stream" in kwargs and kwargs["stream"] is True:
-                    yield from wrapped(*args, **kwargs)
-                    run_response = agent.run_response
-                else:
-                    response = wrapped(*args, **kwargs)
-                    for run_response in response:
-                        # We choose not to double-count tokens here
-                        # Because another instrumentor may be providing token counts
-                        # We might want to make this configurable in the future
-
-                        # input_tokens = run_response.metrics.get("input_tokens", [])
-                        # output_tokens = run_response.metrics.get("output_tokens", [])
-                        # total_tokens = run_response.metrics.get("total_tokens", [])
-                        # span.set_attribute(LLM_TOKEN_COUNT_PROMPT, input_tokens)
-                        # span.set_attribute(LLM_TOKEN_COUNT_COMPLETION, output_tokens)
-                        # span.set_attribute(
-                        #     LLM_TOKEN_COUNT_TOTAL,
-                        #     total_tokens,
-                        # )
-                        span.set_status(trace_api.StatusCode.OK)
-                        span.set_attribute(OUTPUT_VALUE, run_response.to_json())
-                        yield run_response
+                yield from wrapped(*args, **kwargs)
+                run_response = agent.run_response
+                span.set_status(trace_api.StatusCode.OK)
+                span.set_attribute(OUTPUT_VALUE, run_response.to_json())
 
             except Exception as e:
                 span.set_status(trace_api.StatusCode.ERROR, str(e))
@@ -179,11 +205,51 @@ class _RunWrapper:
         kwargs: Mapping[str, Any],
     ) -> Any:
         if context_api.get_value(context_api._SUPPRESS_INSTRUMENTATION_KEY):
-            if "stream" in kwargs and kwargs["stream"] is True:
-                async for response in await wrapped(*args, **kwargs):
-                    yield response
-            else:
-                response = await wrapped(*args, **kwargs)
+            response = await wrapped(*args, **kwargs)
+            return response
+
+        agent = instance
+        if hasattr(agent, "name"):
+            agent_name = agent.name.replace(" ", "_").replace("-", "_")
+        else:
+            agent_name = "Agent"
+        span_name = f"{agent_name}.run"
+
+        with self._tracer.start_as_current_span(
+            span_name,
+            attributes=dict(
+                _flatten(
+                    {
+                        OPENINFERENCE_SPAN_KIND: AGENT,
+                        INPUT_VALUE: _get_input_value(
+                            wrapped,
+                            *args,
+                            **kwargs,
+                        ),
+                        **dict(_agent_run_attributes(agent)),
+                        **dict(get_attributes_from_context()),
+                    }
+                )
+            ),
+        ) as span:
+            try:
+                run_response = await wrapped(*args, **kwargs)
+                span.set_status(trace_api.StatusCode.OK)
+                span.set_attribute(OUTPUT_VALUE, run_response.to_json())
+                return run_response
+            except Exception as e:
+                span.set_status(trace_api.StatusCode.ERROR, str(e))
+                raise
+
+    async def arun_stream(
+        self,
+        wrapped: Callable[..., Awaitable[Any]],
+        instance: Any,
+        args: Tuple[Any, ...],
+        kwargs: Mapping[str, Any],
+    ) -> Any:
+        if context_api.get_value(context_api._SUPPRESS_INSTRUMENTATION_KEY):
+            async for response in await wrapped(*args, **kwargs):
                 yield response
 
         agent = instance
@@ -211,26 +277,11 @@ class _RunWrapper:
             ),
         ) as span:
             try:
-                if "stream" in kwargs and kwargs["stream"] is True:
-                    span.set_status(trace_api.StatusCode.OK)
-                    async for response in await wrapped(*args, **kwargs):
-                        yield response
-                else:
-                    run_response = await wrapped(*args, **kwargs)
-                    span.set_attribute(
-                        LLM_TOKEN_COUNT_PROMPT, sum(run_response.metrics.get("input_tokens", []))
-                    )
-                    span.set_attribute(
-                        LLM_TOKEN_COUNT_COMPLETION,
-                        sum(run_response.metrics.get("output_tokens", [])),
-                    )
-                    span.set_attribute(
-                        LLM_TOKEN_COUNT_TOTAL,
-                        sum(run_response.metrics.get("total_tokens", [])),
-                    )
-                    span.set_status(trace_api.StatusCode.OK)
-                    span.set_attribute(OUTPUT_VALUE, run_response.to_json())
-                    yield run_response
+                async for response in wrapped(*args, **kwargs):
+                    yield response
+                run_response = agent.run_response
+                span.set_status(trace_api.StatusCode.OK)
+                span.set_attribute(OUTPUT_VALUE, run_response.to_json())
             except Exception as e:
                 span.set_status(trace_api.StatusCode.ERROR, str(e))
                 raise
