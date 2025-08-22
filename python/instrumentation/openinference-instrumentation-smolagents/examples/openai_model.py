e615d3bdcbc85e0ef0ad34d81d3be34bbc7ffd8f
Alexander Song <axiomofjoy@gmail.com>
2025-08-22 15:18:56 -0400

    revert

--- python/instrumentation/openinference-instrumentation-smolagents/examples/openai_model.py
+++ python/instrumentation/openinference-instrumentation-smolagents/examples/openai_model.py
@@ -1,7 +1,4 @@
-from phoenix.otel import register
 from smolagents import OpenAIServerModel
-
-register(auto_instrument=True)
 
 model = OpenAIServerModel(model_id="gpt-4o")
 output = model(messages=[{"role": "user", "content": "hello world"}])
