560308e98eddf24b5922708f0fc45124790f418a
srinivasu <srinivasu.r@turing.com>
2025-10-02 18:49:04 +0530

    Typo Fix

--- python/instrumentation/openinference-instrumentation-bedrock/examples/bedrock_converse_stream.py
+++ python/instrumentation/openinference-instrumentation-bedrock/examples/bedrock_converse_stream.py
@@ -40,7 +40,6 @@ def sanitize_format(fmt: str) -> str:
 
 def image_with_stream():
     model_id = "anthropic.claude-3-5-sonnet-20240620-v1:0"
-    model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
     input_text = "What's in this image?"
     img_url = (
         "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/"
