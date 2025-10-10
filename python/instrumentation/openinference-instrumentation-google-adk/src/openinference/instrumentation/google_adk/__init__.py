069b6a1e515214bd16b0c209d13c94c26c49cc25
Roger Hu <rogerhu1989@gmail.com>
2025-10-09 18:27:50 -0700

    adk-test

--- python/instrumentation/openinference-instrumentation-google-adk/src/openinference/instrumentation/google_adk/__init__.py
+++ python/instrumentation/openinference-instrumentation-google-adk/src/openinference/instrumentation/google_adk/__init__.py
@@ -144,7 +144,7 @@ class GoogleADKInstrumentor(BaseInstrumentor):  # type: ignore
 
             setattr(runners, "tracer", _PassthroughTracer(tracer))
 
-        from google.adk.agents.base_agent import (  # type: ignore[attr-defined]
+        from google.adk.agents.base_agent import (  # type: ignore[attr-defined, unused-ignore]
             tracer,  # pyright: ignore[reportPrivateImportUsage]
         )
 
@@ -164,7 +164,7 @@ class GoogleADKInstrumentor(BaseInstrumentor):  # type: ignore
 
             setattr(runners, "tracer", original)
 
-        from google.adk.agents.base_agent import (  # type: ignore[attr-defined]
+        from google.adk.agents.base_agent import (  # type: ignore[attr-defined, unused-ignore]
             tracer,  # pyright: ignore[reportPrivateImportUsage]
         )
 
