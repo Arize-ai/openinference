7a0b16f4f9016ffd0b26ff2402182bcdbb7d21db
Mikyo King <mikyo@arize.com>
2025-10-24 18:36:57 -0600

    switch to vitest for bedrock agent runtime

--- js/packages/openinference-instrumentation-bedrock-agent-runtime/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src", "<rootDir>/test"],
-  testMatch: ["**/*.test.ts"],
-  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
-  prettierPath: null,
-  testTimeout: 50000, // sets timeout to 50 seconds
-};
