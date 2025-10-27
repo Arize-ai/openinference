0afdc251fd61e355c9d698bc5a8d22168d1c6b09
Mikyo King <mikyo@arize.com>
2025-10-27 17:44:37 -0600

    fix: add json type specification for JSON imports in fixtures

--- js/packages/openinference-genai/test/OpenInferenceSpanConverter.test.ts
+++ js/packages/openinference-genai/test/OpenInferenceSpanConverter.test.ts
@@ -1,7 +1,7 @@
-import otelGenaiClientSpanToolCalls from "./__fixtures__/otel_genai_client_span_tool_calls.json";
-import openinferenceClientSpanToolCalls from "./__fixtures__/openinference_client_span_tool_calls.json";
-import openinferenceClientSpanInputOutputOnly from "./__fixtures__/openinference_client_span_input_output_only.json";
-import otelGenaiDeprecatedClientSpanToolCalls from "./__fixtures__/otel_genai_deprecated_client_span_tool_calls.json";
+import otelGenaiClientSpanToolCalls from "./__fixtures__/otel_genai_client_span_tool_calls.json" with { type: "json" };
+import openinferenceClientSpanToolCalls from "./__fixtures__/openinference_client_span_tool_calls.json" with { type: "json" };
+import openinferenceClientSpanInputOutputOnly from "./__fixtures__/openinference_client_span_input_output_only.json" with { type: "json" };
+import otelGenaiDeprecatedClientSpanToolCalls from "./__fixtures__/otel_genai_deprecated_client_span_tool_calls.json" with { type: "json" };
 import { convertGenAISpanAttributesToOpenInferenceSpanAttributes } from "../src/attributes.js";
 
 describe("convertGenAISpanAttributesToOpenInferenceSpanAttributes", () => {
