15c18b05be1ab6abed02d503a6474e29baaa3264
Mikyo King <mikyo@arize.com>
2025-11-14 08:26:19 -0700

    feat: initial langchain legacy instrumentor for JS (#2446)

---
+++ js/.changeset/stupid-wings-rhyme.md
@@ -0,0 +1,5 @@
+---
+"@arizeai/openinference-instrumentation-langchain-v0": patch
+---
+
+Initial publish for langchain 0.X. The main instrumentor will be moving to 1.X
---
+++ js/packages/openinference-instrumentation-langchain-v0/examples/chat.ts
@@ -0,0 +1,32 @@
+import "./instrumentation";
+import "dotenv/config";
+
+import { HumanMessage } from "@langchain/core/messages";
+import { ChatOpenAI } from "@langchain/openai";
+
+const main = async () => {
+  const chatModel = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    metadata: {
+      session_id: "test-session-123",
+    },
+  });
+
+  const request = new HumanMessage("Hello! How are you?");
+
+  const response = await chatModel.invoke([request]);
+
+  // get a new response, including a greeting in the message history
+  const finalResponse = await chatModel.invoke([
+    request,
+    response,
+    new HumanMessage("That is great to hear!"),
+  ]);
+
+  // eslint-disable-next-line no-console
+  console.log(finalResponse.content);
+
+  return finalResponse;
+};
+
+main();
