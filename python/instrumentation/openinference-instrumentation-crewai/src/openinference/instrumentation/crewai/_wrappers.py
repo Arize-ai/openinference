86da05f0fdfe5060dd295de9ceb676bfc2194b20
Roger Hu <rogerhu1989@gmail.com>
2026-02-17 23:04:57 -0800

    ruff

--- python/instrumentation/openinference-instrumentation-crewai/src/openinference/instrumentation/crewai/_wrappers.py
+++ python/instrumentation/openinference-instrumentation-crewai/src/openinference/instrumentation/crewai/_wrappers.py
@@ -221,7 +221,7 @@ class _ExecuteCoreWrapper:
         ) as span:
             span.set_attribute("task_key", instance.key)
             span.set_attribute("task_id", str(instance.id))
-            span.set_attribute("task_name", getattr(instance, 'name', None))
+            span.set_attribute("task_name", getattr(instance, "name", None))
 
             agent = args[0] if args else None
             # Conditionally set attributes for the agent, crew, and task
