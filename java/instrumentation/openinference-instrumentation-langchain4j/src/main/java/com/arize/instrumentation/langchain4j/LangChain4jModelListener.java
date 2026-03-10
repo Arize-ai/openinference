7f2c164ed95b7bd5be385348fb4714abcfe2dcb2
Trevor LaViale <tlaviale@arize.com>
2026-03-09 21:31:17 -0700

    fix import

--- java/instrumentation/openinference-instrumentation-langchain4j/src/main/java/com/arize/instrumentation/langchain4j/LangChain4jModelListener.java
+++ java/instrumentation/openinference-instrumentation-langchain4j/src/main/java/com/arize/instrumentation/langchain4j/LangChain4jModelListener.java
@@ -22,6 +22,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
