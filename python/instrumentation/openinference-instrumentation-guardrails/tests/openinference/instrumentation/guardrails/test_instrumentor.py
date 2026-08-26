2e048b8c9fc48183dfa35d6ef13fc200b3376bb8
Ali Saleh <saleh.a@turing.com>
2026-08-27 00:17:31 +0500

    fix(guardrails): Resolve Failing CI For Testcase Errors

--- python/instrumentation/openinference-instrumentation-guardrails/tests/openinference/instrumentation/guardrails/test_instrumentor.py
+++ python/instrumentation/openinference-instrumentation-guardrails/tests/openinference/instrumentation/guardrails/test_instrumentor.py
@@ -77,7 +77,7 @@ def setup_guardrails_instrumentation(
 
 class TestInstrumentor:
     def test_entrypoint_for_opentelemetry_instrument(self) -> None:
-        (instrumentor_entrypoint,) = entry_points(  # type: ignore[no-untyped-call]
+        (instrumentor_entrypoint,) = entry_points(  # type: ignore[no-untyped-call,unused-ignore]
             group="opentelemetry_instrumentor", name="guardrails"
         )
         instrumentor = instrumentor_entrypoint.load()()
